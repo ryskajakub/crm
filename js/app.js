d9e6437fd49561a5a422d91d2d8bfc53426b3391
Jakub Ryška <ryskajakub@seznam.cz>
2014-09-27 09:14:03 +0200

    add moment and test data

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
+moment.locale("cs");
+
 App = Ember.Application.create();
 
+var companies = [{
+	id: 1
+	, title: "Firma 1"
+	, date: moment(new Date('12-27-2015'))
+}, {
+	id: 2
+	, title: "Firma 2"
+	, date: moment(new Date('1-28-2016'))
+}]
+
 App.Router.map(function() {
-  // put your routes here
+	this.resource('index', { path: '/' });
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+	model: function() {
+		return companies;
+	}
+});
+
+Ember.Handlebars.helper('format-date', function(moment) {
+	return moment.format('LL');
 });
