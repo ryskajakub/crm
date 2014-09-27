688b8d3bbe2c92f472f3d9ce75c63c2570874ba2
Jakub Ryška <ryskajakub@seznam.cz>
2014-09-27 20:53:57 +0200

    add company template

--- js/app.js
+++ js/app.js
@@ -10,15 +10,22 @@ var companies = [{
 	id: 2
 	, title: "Firma 2"
 	, date: moment(new Date('1-28-2016'))
-}]
+}];
 
 App.Router.map(function() {
 	this.resource('index', { path: '/' });
+	this.resource('company', { path: '/company/:company_id' });
 });
 
 App.IndexRoute = Ember.Route.extend({
 	model: function() {
 		return companies;
+	}
+});
+
+App.CompanyRoute = Ember.Route.extend({
+	model: function(params) {
+		return "ahoj " + params.company_id;
 	}
 });
 
