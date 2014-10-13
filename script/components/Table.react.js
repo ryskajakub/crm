265722faecb5428f95248e3af71af0d60bfdf0ca
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-13 20:23:36 +0200

    add basic table to the app

---
+++ script/components/Table.react.js
@@ -0,0 +1,47 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require("react");
+var _ = require("underscore");
+
+var getTable = function () {
+	return {
+		"rows": [1, 2, 4, 5]
+	};
+}
+
+var Table = React.createClass({
+
+  getInitialState: function() {
+		return getTable();
+  },
+
+  /**
+   * @return {object}
+   */
+  render: function() {
+
+		var rows = this.state.rows;
+
+		var rowsHtml = _.reduce(rows, function(acc, elem) {
+			var elem = <tr><td>{elem}</td></tr>;
+			acc.push(elem);
+			return acc;
+		}, []);
+
+  	return (
+			<table>
+				<thead>
+					<tr><th>Column 4</th></tr>
+				</thead>
+				<tbody>
+					{rowsHtml}
+				</tbody>
+			</table>
+  	);
+  }
+
+});
+
+module.exports = Table;
