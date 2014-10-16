3e613522c399cda7a6746cb7895978a11c74c6f1
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-16 16:13:56 +0200

    strip the basic snap server to essence

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -1,24 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Applicative
-import Snap.Core
-import Snap.Util.FileServe
-import Snap.Http.Server
+import Snap.Core (route, writeBS, method, Snap, Method(GET) )
+import Snap.Http.Server (quickHttpServe)
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site =
-  ifTop (writeBS "hello world") <|>
-  route [ ("foo", writeBS "bar")
-    , ("echo/:echoparam", echoHandler)
-    ] <|>
-  dir "static" (serveDirectory ".")
-
-echoHandler :: Snap ()
-echoHandler = do
-  param <- getParam "echoparam"
-  maybe (writeBS "must specify echo/param in URL")
-    writeBS param
+  route [
+    ("foo", method GET (writeBS "bar"))
+  ]
