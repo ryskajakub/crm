1c7f7cf0c4565417cc7a33f68883b1612243a49e
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-16 20:43:58 +0200

    add working example insertion into db

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -3,7 +3,11 @@ module Main where
 
 import Snap.Core (route, writeBS, method, Snap, Method(GET), putResponse, emptyResponse)
 import Snap.Http.Server (quickHttpServe)
+import Database.MySQL.Simple (defaultConnectInfo, Query, query, connect, connectDatabase, execute)
 import Control.Monad.IO.Class (liftIO)
+
+connectionInfo = defaultConnectInfo { connectDatabase = "crm" }
+createCompanyQuery = "insert into Company(name, days) values (?, ?)"
 
 main :: IO ()
 main = quickHttpServe site
@@ -11,8 +15,10 @@ main = quickHttpServe site
 site :: Snap ()
 site =
   route [("/api",
-    route [("/company/new", do 
-      liftIO $ putStrLn "AHOJ" 
+    route [("/company/new", do
+      liftIO $ do
+        connection <- connect connectionInfo
+        execute connection createCompanyQuery ("123" :: String, 345 :: Int)
       method GET $ putResponse emptyResponse
     )]
   )]
---
+++ table-creation.sql
@@ -0,0 +1,6 @@
+create table Company (
+  id mediumint not null auto_increment
+  , name varchar(255) not null
+  , days mediumint not null
+  , primary key(id)
+);
