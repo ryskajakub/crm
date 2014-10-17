81b4dfd955ca1b2af86956ac9afbef7fa7a79acb
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-17 09:16:43 +0200

    reply to malformed body data with correct response code - 400

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -4,7 +4,7 @@
 
 module Main where
 
-import Snap.Core (route, method, Snap, Method(POST), putResponse, emptyResponse, readRequestBody)
+import Snap.Core (route, method, Snap, Method(POST), putResponse, emptyResponse, readRequestBody, setResponseCode, Response)
 import Snap.Http.Server (quickHttpServe)
 import Database.MySQL.Simple (defaultConnectInfo, Query, connect, connectDatabase, execute, close, ConnectInfo)
 import Control.Monad.IO.Class (liftIO)
@@ -31,15 +31,18 @@ main = quickHttpServe site
 site :: Snap ()
 site =
   route [("/api",
-    route [("/company/new", do
-      requestBody <- readRequestBody 1024
-      maybeCompany <- return $ (decode requestBody :: Maybe Company)
-      case maybeCompany of 
-        Just (company) -> liftIO $ do
-          connection <- connect connectionInfo
-          execute connection createCompanyQuery (name company, days company)
-          close connection
-        Nothing -> return ()
-      method POST $ putResponse emptyResponse
+    route [("/company/new",
+      method POST $ do
+        requestBody <- readRequestBody 1024
+        maybeCompany <- return $ (decode requestBody :: Maybe Company)
+        response <- case maybeCompany of
+          Just (company) -> liftIO $ do
+            connection <- connect connectionInfo
+            execute connection createCompanyQuery (name company, days company)
+            close connection
+            return emptyResponse
+          Nothing ->
+            return $ setResponseCode 400 emptyResponse
+        putResponse response
     )]
   )]
