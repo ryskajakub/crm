3d24351109e8b13c8f81b7fac768b62d6ea8e37a
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-18 19:10:30 +0200

    use the longest common prefix of the route

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -39,45 +39,47 @@ executeWithConnection f =
 site :: Snap ()
 site =
   route [("/api",
-    route [("/companies/new",
-      method POST $ do
-        requestBody <- readRequestBody 1024
-        maybeCompany <- return $ (decode requestBody :: Maybe Company)
-        case maybeCompany of
-          Just (company) ->
-            (=<<) (\x -> x) (liftIO $ bracket
-              (connect connectionInfo)
-              (close)
-              (\connection ->
-                let
-                  queryResult = (try $ do
-                    execute connection createCompanyQuery (name company, days company)
-                    insertID connection) :: IO (Either SomeException Word64)
-                  response = (fmap (\qr -> case qr of
-                    Left _ -> putResponse $ setResponseCode 409 emptyResponse :: Snap()
-                    Right recordId ->
-                      let
-                        encodedId = encode $ IdResponse $ fromIntegral recordId
-                      in
-                        writeLBS encodedId
-                    ) queryResult) :: IO (Snap ())
-                in
-                  response
-              ))
-          Nothing -> do
-            logError $ ("Failed to parse: ") `append` (toStrict requestBody)
-            (putResponse $ setResponseCode 400 emptyResponse)
-    ), ("/companies",
-      method GET $ (=<<) (\x -> x) $ liftIO $ do
-        rows <- executeWithConnection (\connection -> do
-          rows <- query_ connection getAllCompaniesQuery
-          return $ toJSON $ hashmapize $ fmap (\(companyId, companyName, companyDays) ->
-            let 
-              key = pack $ show (companyId :: Int) :: Text
-              value = toJSON $ Company {name = companyName, days = companyDays} :: Value
-            in (key, value) :: (Text, Value)
-            ) rows
-          )
-        return $ writeLBS $ encode rows :: IO (Snap ())
+    route [("/companies",
+      route [("/new",
+        method POST $ do
+          requestBody <- readRequestBody 1024
+          maybeCompany <- return $ (decode requestBody :: Maybe Company)
+          case maybeCompany of
+            Just (company) ->
+              (=<<) (\x -> x) (liftIO $ bracket
+                (connect connectionInfo)
+                (close)
+                (\connection ->
+                  let
+                    queryResult = (try $ do
+                      execute connection createCompanyQuery (name company, days company)
+                      insertID connection) :: IO (Either SomeException Word64)
+                    response = (fmap (\qr -> case qr of
+                      Left _ -> putResponse $ setResponseCode 409 emptyResponse :: Snap()
+                      Right recordId ->
+                        let
+                          encodedId = encode $ IdResponse $ fromIntegral recordId
+                        in
+                          writeLBS encodedId
+                      ) queryResult) :: IO (Snap ())
+                  in
+                    response
+                ))
+            Nothing -> do
+              logError $ ("Failed to parse: ") `append` (toStrict requestBody)
+              (putResponse $ setResponseCode 400 emptyResponse)
+      ), ("/companies",
+        method GET $ (=<<) (\x -> x) $ liftIO $ do
+          rows <- executeWithConnection (\connection -> do
+            rows <- query_ connection getAllCompaniesQuery
+            return $ toJSON $ hashmapize $ fmap (\(companyId, companyName, companyDays) ->
+              let
+                key = pack $ show (companyId :: Int) :: Text
+                value = toJSON $ Company {name = companyName, days = companyDays} :: Value
+              in (key, value) :: (Text, Value)
+              ) rows
+            )
+          return $ writeLBS $ encode rows :: IO (Snap ())
+      )]
     )]
   )]
