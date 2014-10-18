8027ae737302f1347dee9462624e9fd02bb4d7d2
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-18 16:56:16 +0200

    test of IdToObject json encoding

--- server/test/MainTestSuite.hs
+++ server/test/MainTestSuite.hs
@@ -1,14 +1,29 @@
 module Main where
 
-import Test.HUnit(Assertion, assertBool)
-import Test.Framework(defaultMainWithOpts)
+import Test.HUnit(Assertion, assertEqual)
+import Test.Framework(defaultMain)
 import Test.Framework.Providers.HUnit(testCase)
-import Data.Monoid(mempty)
 
-sampleTest :: Assertion
-sampleTest = assertBool "x" False
+import Data.Aeson(toJSON, Value(Object), encode)
+
+import Server.Data
+import Debug.Trace(trace)
+
+import Data.Text.Encoding(decodeUtf8)
+import Data.Text(unpack)
+import Data.ByteString.Lazy(toStrict)
+
+companyToJsonTest :: Assertion
+companyToJsonTest = 
+  let
+    objectToJSONify = IdToObject {idValue = 5, object = Company {name = "company 1", days = 30} }
+    asString = unpack $ decodeUtf8 $ toStrict $ encode objectToJSONify
+  in assertEqual
+    "Encoding of `IdToObject` failed" 
+    "{\"5\":{\"days\":30,\"name\":\"company 1\"}}" 
+    asString
 
 main :: IO ()
-main = defaultMainWithOpts
-       [testCase "push-pop" sampleTest]
-       mempty
+main = defaultMain [
+    testCase "Company to json test" companyToJsonTest
+  ]
