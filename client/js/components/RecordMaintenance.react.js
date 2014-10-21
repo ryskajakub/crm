936e0a0e2c92b9c04e94804dccc5dae3ce2e13b0
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-21 20:42:12 +0200

    draft for recording maintenance

--- client/js/components/RecordMaintenance.react.js
+++ client/js/components/RecordMaintenance.react.js
@@ -11,6 +11,13 @@ var B = require("react-bootstrap");
 
 var Table = B.Table;
 var Link = Router.Link;
+var Well = B.Well;
+var Input = B.Input;
+var Button = B.Button;
+var Glyphicon = B.Glyphicon;
+var ButtonToolbar = B.ButtonToolbar;
+var DropdownButton = B.DropdownButton;
+var MenuItem = B.MenuItem;
 
 var DocumentTitle = require('react-document-title');
 
@@ -18,7 +25,22 @@ var RecordMaintenance = React.createClass({
 
   render: function() {
     return (
-      <div>Zapiš servis</div>
+      <Well>
+        <h2>Zaznamenej servis</h2>
+        <h3>ZICO</h3>
+        <strong>Datum: </strong>5.11.2014
+        <div>
+          <strong>Technik: </strong>
+          <ButtonToolbar>
+            <DropdownButton title="vyber">
+              <MenuItem>Kutička</MenuItem>
+              <MenuItem>Mandlík</MenuItem>
+            </DropdownButton>
+          </ButtonToolbar>
+        </div>
+        <Input type="textarea" label="Poznámka" rows="5" />
+        <Button bsStyle="primary"><Glyphicon glyph="ok" /> Zaznamenej servis</Button>
+      </Well>
     );
   }
 
