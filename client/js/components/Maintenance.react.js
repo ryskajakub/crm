bfffb4d92596d18850da8bd7431a89979139b74e
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-23 14:58:02 +0200

    display the company name in the edit maintenance screen #4

--- client/js/components/Maintenance.react.js
+++ client/js/components/Maintenance.react.js
@@ -4,12 +4,13 @@
 var React = require('react');
 
 var Moment = require("../utils/Moment");
-var listenToStoreSimpleMixin = require("../utils/listenToStoreSimpleMixin");
+var listenToStoresMixin = require("../utils/listenToStoresMixin");
 
 var Machine = require("./Machine.react");
 var MaintenanceForm = require("./MaintenanceForm.react");
-
 var MachineStore = require("../stores/MachineStore");
+var CompanyStore = require("../stores/CompanyStore");
+var MaintenanceStore = require("../stores/MaintenanceStore");
 
 var B = require("react-bootstrap");
 var Row = B.Row;
@@ -18,11 +19,24 @@ var _ = require("underscore");
 
 var Maintenance = React.createClass({
 
+/*
   mixins: [listenToStoreSimpleMixin(MachineStore, "machines", function(component, store) {
     return store.getByCompanyId(component.props.params.companyId);
   })]
+*/
+  mixins: [listenToStoresMixin([MachineStore, MaintenanceStore, CompanyStore])]
+
+  , computeStateFromStores: function () {
+    var maintenanceId = this.props.params.maintenanceId;
+    var maintenance = MaintenanceStore.get(maintenanceId);
+    var company = CompanyStore.get(maintenance["companyId"]);
+    return ({"maintenance": maintenance, "company": company});
+  }
 
   , render: function() {
+
+    var maintenance = this.state["maintenance"];
+    var company = this.state["company"];
 
     var machines = _.reduce(this.state.machines, function (acc, value, key) {
       var element = <Machine key={key} machine={value} />
@@ -33,7 +47,7 @@ var Maintenance = React.createClass({
     return (
       <main>
         <h1>Servis</h1>
-        <h2>Continental</h2>
+        <h2>{company["name"]}</h2>
         <Row>{machines}</Row>
         <MaintenanceForm />
       </main>
