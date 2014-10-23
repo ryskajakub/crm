94c2cfdc51cb3a85cf9cb0bb0e6596d3502ac44e
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-23 11:35:42 +0200

    add the possibility to listen to more stores #4

--- client/js/utils/listenToStoreMixin.js
+++ client/js/utils/listenToStoreMixin.js
@@ -1,29 +1,67 @@
+var _ = require("underscore");
+
 function listenToStoresMixin(store, resultName, fetch) {
 
-  var StoreMixin = {
-    getInitialState: function() {
-      var result = {}
-      result[resultName] = fetch(this, store);
-      return result;
-    }
+  var numArguments =  _.size(arguments);
 
-    , componentDidMount: function() {
-      store.addChangeListener(this.handleStoresChanged);
-      this.setState(this.getInitialState());
-    }
-
-    , componentWillUnmount: function() {
-      store.removeChangeListener(this.handleStoresChanged);
-    }
-
-    , handleStoresChanged: function() {
-      if (this.isMounted()) {
-        this.setState(getInitialState());
+  if (numArguments === 3) {
+    var StoreMixin = {
+      getInitialState: function() {
+        var result = {}
+        result[resultName] = fetch(this, store);
+        return result;
       }
-    }
-  };
 
-  return StoreMixin;
+      , componentDidMount: function() {
+        store.addChangeListener(this.handleStoresChanged);
+        this.setState(this.getInitialState());
+      }
+
+      , componentWillUnmount: function() {
+        store.removeChangeListener(this.handleStoresChanged);
+      }
+
+      , handleStoresChanged: function() {
+        if (this.isMounted()) {
+          this.setState(getInitialState());
+        }
+      }
+    };
+    return StoreMixin;
+  } else if (numArguments === 1) {
+
+    // stores in array
+    var stores = arguments[0];
+
+    // depends on computeStateFromStores()
+    var StoreMixin = {
+      getInitialState: function() {
+        return this.computeStateFromStores();
+      }
+
+      , handleStoresChanged: function() {
+        if (this.isMounted()) {
+          this.setState(this.computeStateFromStores());
+        }
+      }
+
+      , componentDidMount: function() {
+        var t = this;
+        _.forEach(stores, function(store) {
+          store.addChangeListener(t.handleStoresChanged);
+        });
+        this.setState(this.computeStateFromStores());
+      }
+
+      , componentWillUnmount: function() {
+        var t = this;
+        _.forEach(stores, function(store) {
+          store.removeChangeListener(t.handleStoresChanged);
+        });
+      }
+    };
+    return StoreMixin;
+  }
 }
 
 module.exports = listenToStoresMixin;
