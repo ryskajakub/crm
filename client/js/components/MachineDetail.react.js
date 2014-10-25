1296a5530fab77ae5fe3ef14c2ab770824c1ff76
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-25 20:46:35 +0200

    add input field for type on the new machine page #16

--- client/js/components/MachineDetail.react.js
+++ client/js/components/MachineDetail.react.js
@@ -3,10 +3,36 @@
  */
 var React = require('react');
 
+var B = require("react-bootstrap");
+var Grid = B.Grid;
+var Col = B.Col;
+var Row = B.Row;
+var Input = B.Input;
+
 var MachineDetail = React.createClass({
 
   render: function() {
-    return(<span/>);
+    return(
+      <Grid>
+        <Row>
+          <Col mdOffset={3} md={6}>
+            <form className="form-horizontal relative">
+              <Row>
+                <Col mdOffset={2} md={10}>
+                  <h1>Nové zařízení</h1>
+                </Col>
+              </Row>
+              <Input type="text" label="Typ" onChange={this.changeTypeText}
+                labelClassName="col-md-2" wrapperClassName="col-md-10" groupClassName="row" />
+            </form>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+
+  , changeTypeText: function(event) {
+    var value = event.target.value;
   }
 
 });
