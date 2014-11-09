b233cceff027d2c33d8cf7119db9ad436d3d3e0f
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-09 10:34:02 +0100

    list services for company #17

--- client/js/components/MaintenancesList.react.js
+++ client/js/components/MaintenancesList.react.js
@@ -4,21 +4,42 @@
 var React = require('react');
 var listenToStoresMixin = require("../utils/listenToStoresMixin");
 var MaintenanceStore = require("../stores/MaintenanceStore");
+var EmployeeStore = require("../stores/EmployeeStore");
+
+var formatDate = require("../utils/formatDate");
+
+var B = require("react-bootstrap");
 
 var MaintenancesList = React.createClass({
 
-  mixins: [listenToStoresMixin([MaintenanceStore])]
+  mixins: [listenToStoresMixin([MaintenanceStore, EmployeeStore])]
 
   , computeStateFromStores: function() {
     var companyId = this.props.params.companyId
     var maintenances = MaintenanceStore.getByCompanyId(companyId);
-    return {"maintenances": maintenances};
+    var maintenancesWithEmployeeName = _.map(maintenances, function(maintenance) {
+      var employee = EmployeeStore.get(maintenance.employeeId);
+      maintenance["employeeName"] = employee["name"];
+      return maintenance;
+    });
+    return {"maintenances": maintenancesWithEmployeeName};
   }
 
   , render: function () {
-    var maintenances = this.state.maintenances;
+    var maintenances = _.map(this.state.maintenances, function (obj, key) {
+      var date = formatDate(obj.date);
+      var person = obj.employeeName;
+      return <tr key={key}><td>{date}</td><td>{person}</td></tr>;
+    });
     return (
-      <div/>
+      <B.Table striped bordered>
+        <thead>
+          <tr><th>Datum</th><th>Servisman</th></tr>
+        </thead>
+        <tbody>
+          {maintenances}
+        </tbody>
+      </B.Table>
     );
   }
 });
