5ff488f5d5094738224000a441d348af52499917
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-12 14:36:21 +0100

    add all instances needed for (de)serialization #24

--- server/src/Server.hs
+++ server/src/Server.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -8,6 +9,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Server (
   api, main
@@ -19,16 +22,19 @@ import Control.Monad.Error (ErrorT(ErrorT), Error)
 import Control.Monad.Reader (ReaderT, ask, mapReaderT, runReaderT)
 
 import Data.Text (pack, Text)
+import Data.JSON.Schema.Generic (gSchema)
+import qualified Data.JSON.Schema.Types as JS (JSONSchema(schema))
+import Data.Typeable.Internal (Typeable)
 
 import Snap.Http.Server (quickHttpServe)
 import Snap.Core (Snap)
 
 import Rest.Api (Api, mkVersion, Some1(Some1), Router, route, root, compose)
 import Rest.Driver.Snap (apiToHandler')
-import Rest.Resource (Resource, mkResourceId, Void, name, schema, list)
+import Rest.Resource (Resource, mkResourceId, Void, name, schema, list, create)
 import Rest.Schema (Schema, named, withListing)
-import Rest.Dictionary.Combinators (jsonO, someO)
-import Rest.Handler (ListHandler, mkListing)
+import Rest.Dictionary.Combinators (jsonO, someO, jsonI, someI)
+import Rest.Handler (ListHandler, mkListing, Handler, mkInputHandler)
 import Rest.Types.Error (Reason)
 
 import Database.Persist (insert_, delete, deleteWhere, selectList, (==.), SelectOpt(LimitTo), get, Entity)
@@ -36,41 +42,61 @@ import Database.Persist.Sql (ConnectionPool)
 import Database.Persist.Postgresql (withPostgresqlPool, runMigration, runSqlPersistMPool)
 import Database.Persist.TH (mkPersist, mkMigrate, share, sqlSettings, persistLowerCase)
 
+import Generics.Regular
+import GHC.Generics
+
+import Debug.Trace(trace)
+import Data.Aeson.Types (toJSON)
+import Data.Aeson (encode)
+
 type Dependencies = (ReaderT ConnectionPool IO :: * -> *)
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Company
+Company json
   name String
   plant String
   contact String
   phone String
   address String
-  deriving Show
+  deriving Show Typeable Generic
 |]
+
+deriveAll ''Company "PFCompany"
+type instance PF Company = PFCompany
+
+instance JS.JSONSchema Company where schema = gSchema
 
 performDb :: (Error a) => (ConnectionPool -> IO()) -> ErrorT a (ReaderT ConnectionPool IO) ()
 performDb action = ask >>= \pool -> liftIO $ action pool
 
-insertPerson :: ConnectionPool -> IO ()
-insertPerson = runSqlPersistMPool $ do
+insertCompany :: Company -> ConnectionPool -> IO ()
+insertCompany company = runSqlPersistMPool $ do
   runMigration migrateAll
-  insert_ $ Company "Continental" "I" "p.Jelínek" "721 650 194" "Brandýs nad labem"
+  insert_ $ company
 
 listing' :: ListHandler Dependencies
-listing' = mkListing (jsonO . someO) (const $ (performDb insertPerson) >> return [pack "XXX"])
+listing' = mkListing (jsonO . someO) (const $ (performDb $ insertCompany $ let
+  c = Company "Continental" "I" "p.Jelínek" "721 650 194" "Brandýs nad labem"
+  json = encode $ toJSON c
+  in trace(show json)(c)
+  ) >> return [pack "XXX"])
 
-dogSchema :: Schema Void () Void
-dogSchema = withListing () (named [])
+companySchema :: Schema Void () Void
+companySchema = withListing () (named [])
 
-dog' :: Resource Dependencies Dependencies Void () Void
-dog' = mkResourceId {
+companyCreate :: Handler Dependencies
+companyCreate = mkInputHandler (jsonI . someI) (\company -> performDb $ insertCompany company)
+
+companyResource :: Resource Dependencies Dependencies Void () Void
+companyResource = mkResourceId {
   list = const listing'
-  , name = "dogs"
-  , schema = dogSchema
+  , name = "company"
+  , schema = companySchema
+  , create = Just companyCreate
   }
 
 router' :: Router Dependencies Dependencies
-router' = root `compose` (route dog')
+router' = root `compose` (route companyResource)
 
 api :: Api Dependencies
 api = [(mkVersion 1 0 0, Some1 $ router')]
