851554f50a9e929fea136d3714f1a3ca70ef2728
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-14 11:44:44 +0100

    simply pass all the data to react class

--- React-Wrapper.js
+++ React-Wrapper.js
@@ -2,11 +2,7 @@ var constructDOMElement = function(elementName, attributes, children) {
   return React.DOM[elementName](attributes, children);
 }
 var declareReactClass = function(r) {
-  return React.createClass({
-    render: r.render
-    , displayName: r.displayName
-    , componentDidMount: r.componentDidMount
-  });
+  return React.createClass(r);
 }
 var renderReact = function(component) {
   React.renderComponent (
