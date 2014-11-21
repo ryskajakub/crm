589e04ce6d428cc6ed6e6460494106a9d41b3e0a
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-21 21:38:38 +0100

    delete the files that belongs to different project

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,9 @@
 var gulp = require("gulp");
 var shell = require("gulp-shell");
-var concat = require("gulp-concat");
 var clean = require('gulp-clean');
 var webpack = require('gulp-webpack');
 
 var faySources = 'src/**/*.hs'
-
-// main
-
-gulp.task('copy-resources', ['copy-bootstrap'], function() {
-  return gulp.src(['files/*.html'])
-    .pipe(gulp.dest('build/'));
-});
-
-gulp.task('copy-bootstrap', function () {
-  return gulp.src(['bootstrap/**/*'], {base: 'bootstrap'})
-    .pipe(gulp.dest('build/bootstrap'));
-});
-
-gulp.task('compile', function() {
-  return gulp.src('src/Main.hs', {read: false})
-    .pipe(shell([
-      "fay --Wall --pretty <%= file.path %> --include src/ --output tmp/HaskellReact.js"
-    ]));
-});
-
-gulp.task('webpack', ['compile', 'copy-resources'], function () {
-  return gulp.src('tmp/HaskellReact.js', {read: false})
-    .pipe(webpack({
-      entry: "./tmp/HaskellReact.js"
-      , output: {
-        filename: "haskell-react-packed.js"
-      }
-    }))
-    .pipe(gulp.dest('build/'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch([faySources, 'files/*.html', 'files/*.js'], ['webpack']);
-});
-
-// test
 
 gulp.task('test-compile', function () {
   return gulp.src('test/*.hs', {read: false})
@@ -72,6 +35,6 @@ gulp.task('test-watch', function() {
 // other
 
 gulp.task('clean', function () {
-  return gulp.src(['test_build/', 'build/'], {read: false})
+  return gulp.src(['test_build/'], {read: false})
     .pipe(clean());
 });
