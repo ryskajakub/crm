44f23021080138b5b2ff5cb21b614e27af2ae50c
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-23 17:07:40 +0100

    add some router code, adjust the deps for that

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -1,4 +1,37 @@
+{-# LANGUAGE PackageImports #-}
+
 module Main where
 
+import HaskellReact
+import "fay-base" Data.Text (pack)
+import Prelude hiding (span, div, elem)
+import HaskellReact.Router
+import HaskellReact.ReadFay (readFayReturn)
+
 main :: Fay ()
-main = putStrLn "hi folks!"
+main = router
+
+data Empty = Empty {}
+
+data' :: ReactData Empty a
+data' = defaultReactData (Empty {}) (
+  const $ readFayReturn $
+    div [
+      span $ pack "AAAAAA"
+      , span $ reactRouter "RouteHandler" (Empty {}) ([] :: [DOMElement])
+    ]
+  )
+
+navigation :: ReactClass a
+navigation = declareReactClass data'
+
+router :: Fay ()
+router = runRouter $
+  reactRouter "Route" (RouteData "/" $ navigation) (
+    reactRouter "Route" (RouteData "users" $ declareInReact $ div $ pack "LLL") ([] :: [DOMElement])
+  )
+
+declareInReact :: DOMElement -> ReactClass a
+declareInReact element = declareReactClass $ (defaultReactData
+  (Empty {})
+  (const $ readFayReturn element))
