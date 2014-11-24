c7b7b7b30674da38ee44238277b6c32cb0bca0ba
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-24 12:01:43 +0100

    add navigation with two links #25

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -6,32 +6,24 @@ import HaskellReact
 import "fay-base" Data.Text (pack)
 import Prelude hiding (span, div, elem)
 import HaskellReact.Router
-import HaskellReact.ReadFay (readFayReturn)
+import Crm.Component.Navigation (navigation)
+import FFI (Defined(Defined, Undefined))
 
 main :: Fay ()
 main = router
 
-data Empty = Empty {}
-
-data' :: ReactData Empty a
-data' = defaultReactData (Empty {}) (
-  const $ readFayReturn $
-    div [
-      span $ pack "AAAAAA"
-      , span $ reactRouter "RouteHandler" (Empty {}) ([] :: [DOMElement])
-    ]
-  )
-
-navigation :: ReactClass a
-navigation = declareReactClass data'
-
 router :: Fay ()
 router = runRouter $
-  route (RouteData "/" $ navigation) (
-    route (RouteData "users" $ declareInReact $ div $ pack "LLL") ([] :: [DOMElement])
-  )
+  route (RouteData (Defined "/") navigation Undefined) [
+    route (RouteData (Defined "users") (declareInReact $ div $ pack "users") (Defined $ pack "users")) 
+      ([] :: [DOMElement])
+    , defaultRoute $ defaultRouteProps 
+        (Defined $ pack "default") 
+        (declareInReact $ div $ pack "default")
+  ]
 
 declareInReact :: DOMElement -> ReactClass a
 declareInReact element = declareReactClass $ (defaultReactData
+  (pack "DeclareInReact")
   (Empty {})
   (const $ readFayReturn element))
