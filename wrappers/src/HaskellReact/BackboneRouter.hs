ac96e8ae560f7fff740adbc1546560cccc4d2823
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-26 12:58:01 +0100

    add totally ffiish backbone router wrapper

--- wrappers/src/HaskellReact/BackboneRouter.hs
+++ wrappers/src/HaskellReact/BackboneRouter.hs
@@ -1,10 +1,19 @@
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RebindableSyntax #-}
-
 module HaskellReact.BackboneRouter where
 
-import HaskellReact (foreignReact, Renderable, CommonJSModule, ReactInstance, Empty (Empty))
-import FFI (ffi, Automatic, Defined)
-import "fay-base" Data.Text (fromString, Text)
+import FFI (ffi)
 
+startRouter :: Fay ()
+startRouter = ffi " \
+\ (function () {\
+  \ console.log('component mounted');\
+  \ var Backbone = require('backbone');\
+  \ var router = new Backbone.Router({\
+    \ routes: {\
+      \ 'help/:param/:param2': function(param2, paramxxx) {\
+        \ console.log(param2, paramxxx);\
+      \ }\
+    \ } \
+  \ });\
+  \ Backbone.history.start();\
+\ })()\
+\ "
