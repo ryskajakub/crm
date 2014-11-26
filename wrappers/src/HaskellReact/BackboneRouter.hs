9c40f719377092e9c0be93d64327021d154dc20c
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-26 13:31:30 +0100

    backbone-router: ability to add routes from the Fay

--- wrappers/src/HaskellReact/BackboneRouter.hs
+++ wrappers/src/HaskellReact/BackboneRouter.hs
@@ -1,18 +1,23 @@
+{-# LANGUAGE PackageImports #-}
+
 module HaskellReact.BackboneRouter where
 
+import "fay-base" Data.Text (Text)
 import FFI (ffi)
 
-startRouter :: Fay ()
+-- | Starts the Backbone router
+startRouter :: [(Text, [Text] -> Fay ())] -- ^ list of routes and callback handlers, the argument to the callback is the params of the route
+            -> Fay ()
 startRouter = ffi " \
 \ (function () {\
-  \ console.log('component mounted');\
   \ var Backbone = require('backbone');\
+  \ var routesFromFay = %1;\
+  \ var routes = {};\
+  \ for (var i = 0; i < routesFromFay.length; i++){\
+    \ routes[routesFromFay[i][0]] = (function(i) { return function() { routesFromFay[i][1](arguments); }; })(i)\
+  \ }\
   \ var router = new Backbone.Router({\
-    \ routes: {\
-      \ 'help/:param/:param2': function(param2, paramxxx) {\
-        \ console.log(param2, paramxxx);\
-      \ }\
-    \ } \
+    \ routes: routes\
   \ });\
   \ Backbone.history.start();\
 \ })()\
