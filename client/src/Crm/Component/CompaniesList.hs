04f1bfdeeca0993e823ff3f9c8be0c3b129dffd0
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-27 09:59:34 +0100

    make use of bootstrap glyphicons

--- client/src/Crm/Component/CompaniesList.hs
+++ client/src/Crm/Component/CompaniesList.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE PackageImports #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RebindableSyntax #-}
 
 module Crm.Component.CompaniesList (
   companiesList
@@ -6,13 +8,21 @@ module Crm.Component.CompaniesList (
 
 import HaskellReact
 import Crm.Component.Navigation (navigation)
-import "fay-base" Data.Text (pack)
+import "fay-base" Data.Text (fromString)
 import Prelude hiding (div)
 import HaskellReact.BackboneRouter (BackboneRouter)
+import qualified HaskellReact.Bootstrap as B
+import qualified HaskellReact.Bootstrap.Glyphicon as G
 
 companiesList :: Maybe BackboneRouter -- ^ Router from which the link can be created
               -> ReadFay DOMElement
 companiesList router = readFayReturn $ let 
-  element = div $ pack ("Companies list")
+  element = main $
+    section [
+      B.button [
+        G.plus
+        , text2DOM "Přidat firmu"
+      ]
+    ]
   elementWithNavigation = reactInstance2DOM $ classInstance $ navigation router element
   in elementWithNavigation
--- client/src/Crm/Component/Navigation.hs
+++ client/src/Crm/Component/Navigation.hs
@@ -9,13 +9,14 @@ import "fay-base" Data.Text (pack, Text, fromString)
 import Prelude hiding (span, div, elem)
 import HaskellReact.Bootstrap (navBar, nav)
 import HaskellReact.BackboneRouter (link, BackboneRouter)
+import qualified HaskellReact.Bootstrap.Glyphicon as G
 
 navigation :: Maybe BackboneRouter -> DOMElement -> ReactClass a
 navigation router innerElement = declareReactClass $ reactData ("Navigation") (Empty {}) (
   \reactThis -> readFayReturn $
     div [
       reactInstance2DOM $ navBar $ nav [
-        li $ link "Seznam firem" "" router
+        li $ link [G.list, text2DOM " Seznam firem"] "" router
       ]
       , innerElement
     ]
