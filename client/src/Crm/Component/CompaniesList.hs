79dc4f3009c484b033fc7724c0419eb4870aea5e
Jakub Ryška <ryskajakub@seznam.cz>
2014-11-27 10:13:46 +0100

    print some dummy rows on companies list page #27

--- client/src/Crm/Component/CompaniesList.hs
+++ client/src/Crm/Component/CompaniesList.hs
@@ -8,20 +8,45 @@ module Crm.Component.CompaniesList (
 
 import HaskellReact
 import Crm.Component.Navigation (navigation)
-import "fay-base" Data.Text (fromString)
+import "fay-base" Data.Text (fromString, Text)
 import Prelude hiding (div)
 import HaskellReact.BackboneRouter (BackboneRouter)
 import qualified HaskellReact.Bootstrap as B
 import qualified HaskellReact.Bootstrap.Glyphicon as G
 
+data Company = Company {
+  companyName :: Text
+  , nextMaintenance :: Text
+}
+
+companies :: [Company]
+companies = [
+  Company "F 1" "50 dní"
+  , Company "F 2" "2 roky"
+  , Company "F 5" "8 roků"
+  ]
+
 companiesList :: Maybe BackboneRouter -- ^ Router from which the link can be created
               -> ReadFay DOMElement
 companiesList router = readFayReturn $ let 
-  element = main $
-    section [
+  mkRow company = tr [
+    td $ companyName company
+    , td $ nextMaintenance company
+    ]
+  rows = map mkRow companies 
+  element = main [
+    section $
       B.button [
         G.plus
         , text2DOM "Přidat firmu"
+      ]
+    , section $ 
+      B.table [
+        thead $ tr [
+          th "Název firmy"
+          , th "Platnost servisu vyprší za"
+        ]
+        , tbody rows
       ]
     ]
   elementWithNavigation = reactInstance2DOM $ classInstance $ navigation router element
