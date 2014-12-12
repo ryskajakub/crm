7970385fec8debff000c87df6326deb22c180389
Jakub Ryška <ryskajakub@seznam.cz>
2014-12-12 13:58:03 +0100

    abandon the lifecycle stuff

--- client/src/NoMain.hs
+++ client/src/NoMain.hs
@@ -2,64 +2,46 @@
 
 module NoMain where
 
-import HaskellReact hiding (main)
+import HaskellReact hiding (main, id)
+import HaskellReact.Tag.Input
 import Prelude hiding (span, div, elem)
 import Data.Nullable (fromNullable)
-import Data.Var (Var, newVar)
-import FFI (ffi, Nullable)
-import "fay-base" Data.Text (Text, pack)
+import Data.Var (Var, newVar, subscribeAndRead, set, oneShot, get, waitFor, modify)
+import FFI (ffi, Nullable, Defined(Defined))
+import "fay-base" Data.Text (Text, pack, unpack, append)
 
-import HaskellReact.BackboneRouter (startRouter, BackboneRouter)
-import Crm.Component.CompaniesList (companiesList)
-import Crm.Component.CompanyDetail (companyDetail)
-import Crm.Component.Navigation
-import Crm.Component.Data
+import HaskellReact.BackboneRouter (startRouter, BackboneRouter, link)
 import Crm.Shared.Data
 import Crm.Server (fetchFromServer)
 
 import "fay-base" Debug.Trace
 
-mainStartState :: MainState
-mainStartState = MainState {
-  routerState = CompaniesList
-  , router = Nothing
-}
-
-modifyMainState :: RouterState -> MainState -> MainState
-modifyMainState routerState' mainState = mainState {
-  routerState = routerState'
-}
-
 main' :: Fay ()
 main' = do
-  companiesVar' <- companiesVar
-  fetchFromServer companiesVar'
-  placeElementToBody $ classInstance $ declareReactClass $
-    (reactData (pack "CrmRouter") (mainStartState) (\reactThis ->
-      state reactThis `readFayBind` \mainState -> let
-        in case routerState mainState of
-          CompaniesList -> companiesList (router mainState) companiesVar'
-          CompanyDetail cId -> companyDetail (router mainState) companiesVar' cId
-    )) {
-      componentWillMount = \reactThis -> do
-        router' <- startRouter [(pack "", const $ do
-            state' <- (runReadFay $ state reactThis)
-            let newState = modifyMainState CompaniesList state'
-            setState reactThis newState
-          ), (pack "companies/:id", \params -> do
-            state' <- (runReadFay $ state reactThis)
-            let
-              companyIdParsed = fromNullable $ parseInt $ head params
-              newState = maybe state' (\cId ->
-                modifyMainState (CompanyDetail cId) state'
-                ) companyIdParsed
-            setState reactThis newState
-          )]
-        state' <- runReadFay $ state reactThis
-        setState reactThis $ state' {
-          router = Just router'
-        }
-    }
+  routerVar' <- routerVar
+  router <- startRouter [(
+      pack "", const $ set routerVar' FrontPage
+    ), ( 
+      pack "page/:id", \id -> set routerVar' (OtherPage $ head id)
+    )]
+  dataVar' <- dataVar
+  set dataVar' "AAAAAAa"
+  userInputVar' <- userInputVar
+  _ <- subscribeAndRead routerVar' (\navigationState -> let
+    navigation mainElement = simpleReactBody $ div [
+      div [
+        link (pack "Front page") (pack "") router
+        , link (pack "Other page") (pack "page/0") router
+        , div $ pack "Currpage" `append` (pack $ show navigationState)
+        , mainElement
+      ]]
+    in case navigationState of
+      FrontPage -> navigation $ div $ pack "FP"
+      OtherPage someId -> do
+        waitFor dataVar' (\t -> t /= "") (\c -> navigation $ div $ pack c)
+        modify dataVar' id
+    )
+  return ()
 
 parseInt :: Text -> Nullable Int
 parseInt = ffi " (function() { var int = parseInt(%1); ret = ((typeof int) === 'number' && !isNaN(int)) ? int : null; return ret; })() "
@@ -68,4 +50,15 @@ parseSafely :: Text -> Maybe Int
 parseSafely possibleNumber = fromNullable $ parseInt possibleNumber
 
 companiesVar :: Fay (Var [Company])
-companiesVar = newVar [Company 1 "a" "b" "c" ("1") ("2")]
+companiesVar = newVar []
+
+userInputVar :: Fay (Var String)
+userInputVar = newVar "AHOJ"
+
+data NavigationState = FrontPage | OtherPage { id1 :: Text }
+
+routerVar :: Fay (Var NavigationState)
+routerVar = newVar FrontPage
+
+dataVar :: Fay (Var String)
+dataVar = newVar ""
