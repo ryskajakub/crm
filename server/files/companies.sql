fb6f30e6e0cb37dbcfa2a26773fdc03dba3d61c7
Jakub Ryška <ryskajakub@seznam.cz>
2014-12-13 11:59:02 +0100

    create sample data script #29

--- server/files/companies.sql
+++ server/files/companies.sql
@@ -1,16 +1,44 @@
 drop table companies;
-
 create table companies (
   id serial
   , name varchar(500)
   , plant varchar(500)
 );
 
-insert into companies(name, plant) values ('Continental', 'I');
-insert into companies(name, plant) values ('České dráhy', 'I');
-insert into companies(name, plant) values ('FOMA Bohemia', 'I');
+drop table machines;
+create table machines (
+  company_id integer
+  , name varchar(500)
+);
 
-/*
-  companyBase = Company 0 "Continental" "I" "p.Jelínek" "721 650 194" "Brandýs nad labem"
-  companyNames = ["Continental","České dráhy","FOMA Bohemia","Kand","Metrostav","Neumann","PREX","Stachema Kolín","Valsabbia"]
-*/
+with companies as (
+  insert into companies(name, plant) values ('Continental', 'I') returning id
+), machines as (
+  select 'Machina 1' as name
+  union
+  select 'Albert abc' as name
+)
+insert into machines (company_id, name)
+select id, name
+from companies
+cross join machines;
+
+with companies as (
+  insert into companies(name, plant) values ('České dráhy', 'II') returning id
+), machines as (
+  select 'Kompresor 1' as name
+  union
+  select 'Manesman demagog' as name
+  union
+  select 'Atlas copco XXX1' as name
+)
+insert into machines (company_id, name)
+select id, name
+from companies
+cross join machines;
+
+insert into companies(name, plant) values ('FOMA Bohemia', 'I');
+insert into companies(name, plant) values ('Kand', 'I');
+insert into companies(name, plant) values ('Metrostav', 'I');
+insert into companies(name, plant) values ('Neumann', 'I');
+insert into companies(name, plant) values ('PREX', 'I');
