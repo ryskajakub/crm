d0e72c1eb16239963399c26054a081c2c6bfef8c
Jakub Ryška <ryskajakub@seznam.cz>
2014-12-19 22:37:35 +0100

    add `now` and basic opaque data types

--- src/Moment.hs
+++ src/Moment.hs
@@ -1,1 +1,29 @@
-module Moment where
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE PackageImports #-}
+
+module Moment (
+  Moment, MomentObject ,
+  requireMoment, browserMoment ,
+  now ,
+  Moment ) where
+
+import FFI
+import HaskellReact
+import "fay-base" Prelude
+
+-- | opaque data type representing moment library instance
+data MomentObject
+
+-- | opaque data type representing concrete moment in time
+data Moment
+
+-- | use CommonJS module
+requireMoment :: Moment
+requireMoment = ffi " require('moment') "
+
+-- | conjure up the moment from the thin air
+browserMoment :: Moment
+browserMoment = ffi " moment "
+
+now :: Moment -> MomentObject
+now = ffi " %1() "
