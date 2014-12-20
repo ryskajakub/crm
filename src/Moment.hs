107525e6b98cf41c9cca38596058f13d560e910e
Jakub Ryška <ryskajakub@seznam.cz>
2014-12-20 21:34:30 +0100

    year, month, day tuple extraction function

--- src/Moment.hs
+++ src/Moment.hs
@@ -6,10 +6,12 @@ module Moment (
   requireMoment, browserMoment ,
   now ,
   dayPrecision ,
+  day ,
   Moment ) where
 
 import FFI
 import "fay-base" Prelude
+import "fay-base" Data.Text (Text, pack)
 
 -- | opaque data type representing moment library instance
 data MomentObject
@@ -34,3 +36,23 @@ dayPrecision :: Int -- ^ year
              -> Moment
              -> MomentObject
 dayPrecision = ffi " %4().year(%1).month(%2).date(%3) "
+
+data GetDate 
+  = Month
+  | Day
+  | Year
+
+getFFI :: Text -> MomentObject -> Int
+getFFI = ffi " %2[%1]() "
+
+get :: GetDate -> MomentObject -> Int
+get Month = getFFI (pack "month")
+get Year = getFFI (pack "year")
+get Day = getFFI (pack "date")
+
+day :: MomentObject -> (Int, Int, Int)
+day momentObject = let
+  day' = get Day momentObject 
+  month = get Month momentObject
+  year = get Year momentObject
+  in (year, month, day')
