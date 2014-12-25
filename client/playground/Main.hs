4e8c7d33a082b59a4dc3ee5fa7fbdc1b39315abc
Jakub Ryška <ryskajakub@seznam.cz>
2014-12-25 14:01:51 +0100

    use choice as the item in the autocomplete #60

--- client/playground/Main.hs
+++ client/playground/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Main where
 
@@ -9,7 +10,7 @@ import "fay-base" Prelude hiding (span, div, elem)
 import qualified "base" Prelude as BasePrelude
 #endif
 import "fay-base" Data.Var (Var, newVar, subscribeAndRead, get, modify, waitFor)
-import "fay-base" FFI (ffi, Nullable)
+import "fay-base" FFI (ffi, Nullable, Automatic)
 import "fay-base" Data.Text (Text, pack, fromString)
 import "fay-base" Data.Maybe (isJust)
 import HaskellReact hiding (main)
@@ -27,8 +28,23 @@ main = undefined
 
 data JQueryUI
 
+data Request
+
+getTerm :: Request -> Text
+getTerm = ffi " %1['term'] "
+
+data Choice = Choice {
+  label_ :: Text ,
+  value_ :: Text }
+
 data AutocompleteProps = AutocompleteProps {
-  source :: [Text] }
+  source :: Request -> ([Choice] -> Fay ()) -> Fay () }
+
+escape :: Choice -> Choice
+escape = ffi " (function (attributes) { delete attributes['instance']; var escapedAttributes = {}; for (key in attributes) { var newKey = (key.charAt(key.length - 1) == '_' ? key.substring(0, key.length - 1) : key); escapedAttributes[newKey] = attributes[key]; } return escapedAttributes; })(%1) "
+
+escape' :: [Choice] -> [Choice]
+escape' list = map escape list
 
 jQueryUI :: JQueryUI
 jQueryUI = ffi " (function () { var $ = require('jquery'); require('jquery-ui'); return $; })() "
@@ -47,5 +63,7 @@ autocompleteInput = let
   autocomplete = jQueryUIAutocomplete 
     jQueryUI 
     (pack "input")
-    (AutocompleteProps [pack "abc", pack "def", pack "aaaaaaaaaaa"])
+    (AutocompleteProps (\request response -> let
+      term = getTerm request
+      in response $ escape' (Choice term term : [Choice (pack "aaaaaaaaaaa") (pack "bbbb")]) ))
   in (element, autocomplete)
