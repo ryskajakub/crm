19893f2ef90f04092574b97edb7cc814d0f3aa85
Jakub Ryška <ryskajakub@seznam.cz>
2014-12-26 11:20:28 +0100

    adjust helpers api tester #60

--- helpers/create-machine.sh
+++ helpers/create-machine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-STR='{"machineType":{"upkeepPerMileage":10000,"machineTypeName":"BK 150","machineTypeManufacturer":"Remeza","instance":"MachineType"},"companyId":1,"instance":"Machine","machineOperationStartDate":{"precision":{"instance":"DayPrecision"},"year":1970,"month":5,"day":20,"instance":"YearMonthDay"},"initialMileage":100,"mileagePerYear":10000}'
+STR='[{"companyId":1,"instance":"Machine","machineOperationStartDate":{"precision":{"instance":"DayPrecision"},"year":1970,"month":5,"day":20,"instance":"YearMonthDay"},"initialMileage":100,"mileagePerYear":10000},{"slot1":1,"instance":"MyInt"}]'
 curl -X POST -H "Content-Type: application/json" -d "${STR}" http://crm/api/v1.0.0/companies/1/machines/
