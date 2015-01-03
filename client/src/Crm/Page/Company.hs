482c928aaaef0067d5e21cb1ac276d51b2fb2e7e
Jakub Ryška <ryskajakub@seznam.cz>
2015-01-03 10:13:28 +0100

    wipe not needed stuff from new company page #56

--- client/src/Crm/Page/Company.hs
+++ client/src/Crm/Page/Company.hs
@@ -76,7 +76,7 @@ companyNew router var company' = let
     D.navigation = case D.navigation appState of
       cd @ (D.CompanyNew _) -> cd { D.company = modifiedCompany }
       _ -> D.navigation appState })
-  in companyPage editing' router var setCompany company' (-666) saveHandler machines'
+  in companyForm editing' router var setCompany company' saveHandler
 
 companyDetail :: Bool -- ^ is the page editing mode
               -> R.CrmRouter -- ^ common read data
@@ -85,25 +85,13 @@ companyDetail :: Bool -- ^ is the page editing mode
               -> [(Int, M.Machine, Int, MT.MachineType)] -- ^ machines of the company
               -> DOMElement -- ^ company detail page fraction
 companyDetail editing' router var idCompany machines' = let
-  (id', company') = idCompany
+  (companyId, company') = idCompany
   saveHandler =
     R.navigate R.frontPage router
   setCompany modifiedCompany = modify var (\appState -> appState {
     D.navigation = case D.navigation appState of
       cd @ (D.CompanyDetail _ _ _ _) -> cd { D.company = modifiedCompany }
       _ -> D.navigation appState })
-  in companyPage editing' router var setCompany company' id' saveHandler machines'
-
-companyPage :: Bool -- ^ is the page editing mode
-            -> R.CrmRouter -- ^ common read data
-            -> Var D.AppState -- ^ app state var, where the editing result can be set
-            -> (C.Company -> Fay ()) -- ^ modify the edited company data
-            -> C.Company -- ^ company, which data are displayed on this screen
-            -> Int -- ^ company id
-            -> Fay () -- ^ handler called when the user hits save
-            -> [(Int, M.Machine, Int, MT.MachineType)] -- ^ machines of the company
-            -> DOMElement -- ^ company detail page fraction
-companyPage editing' router var setCompany company' companyId saveHandler' machines' = let
   machineBox (machineId', machine', _, machineType) =
     B.col (B.mkColProps 4) $
       B.panel [
@@ -116,68 +104,81 @@ companyPage editing' router var setCompany company' companyId saveHandler' machi
           dt "Další servis" , 
           dd $ displayDate $ M.machineOperationStartDate machine' ] ]
   machineBoxes = map machineBox machines'
+  form = companyForm editing' router var setCompany company' saveHandler
+  upkeepsHistoryRow = B.row $
+    B.col (B.mkColProps 12) $
+      B.panel $
+        span $ R.link 
+          "Historie servisů"
+          (R.maintenances companyId)
+          router
+  machineBoxesRow = B.row (machineBoxes ++ [ let
+    buttonProps = BTN.buttonProps {
+      BTN.onClick = Defined $ const $
+        R.navigate (R.newMachine companyId) router }
+    button = BTN.button' buttonProps [G.plus, text2DOM "Přidat zařízení"]
+    in B.col (B.mkColProps 4) $ B.panel $ h2 $ button ])
+  planUpkeepRow = B.row $
+    B.col (B.mkColProps 12) $
+      B.panel $
+        span $ R.link
+          "Naplánovat servis"
+          (R.newMaintenance companyId)
+          router
   in main [
-    section $ let
-      editButton = let
-        editButtonBody = [G.pencil, HR.text2DOM " Editovat"]
-        editButtonHandler _ = modify var (\appState ->
-          appState {
-            D.navigation = case D.navigation appState of
-              cd @ (D.CompanyDetail _ _ _ _) -> cd { D.editing = True }
-              _ -> D.navigation appState })
-        editButtonProps = BTN.buttonProps {BTN.onClick = Defined editButtonHandler}
-        in BTN.button' editButtonProps editButtonBody
-      headerDisplay = h1 $ pack $ C.companyName company'
-      headerSet newHeader = let
-        company'' = company' {
-          C.companyName = unpack $ newHeader }
-        in setCompany company''
-      header = editable editing' headerDisplay (pack $ C.companyName company') headerSet
-      saveHandler _ = saveHandler'
-      saveEditButton' = BTN.button' (BTN.buttonProps {
-        BTN.onClick = Defined saveHandler , 
-        BTN.bsStyle = Defined "primary" }) "Uložit"
-      saveEditButton = if editing'
-        then [saveEditButton']
-        else []
-      companyBasicInfo = [
-        header , 
-        dl $ [
-          dt "Označení" , 
-          dd $ let
-            plantDisplay = text2DOM $ pack $ C.companyPlant company'
-            setCompanyPlant companyPlant' = let
-              modifiedCompany = company' {
-                C.companyPlant = unpack companyPlant' }
-              in setCompany modifiedCompany
-            in editable editing' plantDisplay (pack $ C.companyPlant company') setCompanyPlant , 
-          dt "Adresa" , 
-          dd "" , 
-          dt "Kontakt" , 
-          dd "" , 
-          dt "Telefon" , 
-          dd "" ] ++ saveEditButton ]
-      companyBasicInfo' = if editing' then companyBasicInfo else editButton:companyBasicInfo
-      in B.jumbotron companyBasicInfo' , 
+    form ,
     section $ B.grid [
-      B.row $
-        B.col (B.mkColProps 12) $
-          B.panel $
-            span $ R.link 
-              "Historie servisů"
-              (R.maintenances companyId)
-              router
-      , B.row (machineBoxes ++ [
-        let
-          buttonProps = BTN.buttonProps {
-            BTN.onClick = Defined $ const $
-              R.navigate (R.newMachine companyId) router }
-          button = BTN.button' buttonProps [G.plus, text2DOM "Přidat zařízení"]
-          in B.col (B.mkColProps 4) $ B.panel $ h2 $ button ])
-      , B.row $
-        B.col (B.mkColProps 12) $
-          B.panel $
-            span $ R.link
-              "Naplánovat servis"
-              (R.newMaintenance companyId)
-              router ] ]
+      upkeepsHistoryRow ,
+      machineBoxesRow ,
+      planUpkeepRow ]]
+
+companyForm :: Bool -- ^ is the page editing mode
+            -> R.CrmRouter -- ^ common read data
+            -> Var D.AppState -- ^ app state var, where the editing result can be set
+            -> (C.Company -> Fay ()) -- ^ modify the edited company data
+            -> C.Company -- ^ company, which data are displayed on this screen
+            -> Fay () -- ^ handler called when the user hits save
+            -> DOMElement -- ^ company detail page fraction
+companyForm editing' router var setCompany company' saveHandler' = 
+  section $ let
+    editButton = let
+      editButtonBody = [G.pencil, HR.text2DOM " Editovat"]
+      editButtonHandler _ = modify var (\appState ->
+        appState {
+          D.navigation = case D.navigation appState of
+            cd @ (D.CompanyDetail _ _ _ _) -> cd { D.editing = True }
+            _ -> D.navigation appState })
+      editButtonProps = BTN.buttonProps {BTN.onClick = Defined editButtonHandler}
+      in BTN.button' editButtonProps editButtonBody
+    headerDisplay = h1 $ pack $ C.companyName company'
+    headerSet newHeader = let
+      company'' = company' {
+        C.companyName = unpack $ newHeader }
+      in setCompany company''
+    header = editable editing' headerDisplay (pack $ C.companyName company') headerSet
+    saveHandler _ = saveHandler'
+    saveEditButton' = BTN.button' (BTN.buttonProps {
+      BTN.onClick = Defined saveHandler , 
+      BTN.bsStyle = Defined "primary" }) "Uložit"
+    saveEditButton = if editing'
+      then [saveEditButton']
+      else []
+    companyBasicInfo = [
+      header , 
+      dl $ [
+        dt "Označení" , 
+        dd $ let
+          plantDisplay = text2DOM $ pack $ C.companyPlant company'
+          setCompanyPlant companyPlant' = let
+            modifiedCompany = company' {
+              C.companyPlant = unpack companyPlant' }
+            in setCompany modifiedCompany
+          in editable editing' plantDisplay (pack $ C.companyPlant company') setCompanyPlant , 
+        dt "Adresa" , 
+        dd "" , 
+        dt "Kontakt" , 
+        dd "" , 
+        dt "Telefon" , 
+        dd "" ] ++ saveEditButton ]
+    companyBasicInfo' = if editing' then companyBasicInfo else editButton:companyBasicInfo
+    in B.jumbotron companyBasicInfo'
