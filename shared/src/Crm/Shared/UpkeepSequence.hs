ae461149eac38d1a4e804c414ccdb85232abfc82
Jakub Ryška <ryskajakub@seznam.cz>
2015-01-09 09:37:37 +0100

    add upkeep sequence datatype to shared/ #78

---
+++ shared/src/Crm/Shared/UpkeepSequence.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Crm.Shared.UpkeepSequence where
+
+#ifndef FAY
+import GHC.Generics
+import "base" Data.Data
+import "base" Prelude
+#else
+import "fay-base" Prelude
+#endif
+
+data UpkeepSequence = UpkeepSequence {
+  displayOrdering :: Int , 
+  label :: String ,
+  repetition :: Int }
+#ifndef FAY
+  deriving (Generic, Typeable, Data, Show)
+#endif
+
+newUpkeepSequence :: UpkeepSequence
+newUpkeepSequence = UpkeepSequence 0 "" 0
