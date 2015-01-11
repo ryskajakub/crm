bfd5c9ba29795d1b6ff56da79dfebd79cff958ef
Jakub Ryška <ryskajakub@seznam.cz>
2015-01-11 18:37:25 +0100

    add disable to the button dropdown

--- wrappers/src/HaskellReact/Bootstrap/ButtonDropdown.hs
+++ wrappers/src/HaskellReact/Bootstrap/ButtonDropdown.hs
@@ -7,12 +7,13 @@ module HaskellReact.Bootstrap.ButtonDropdown where
 
 import "fay-base" Prelude hiding (div)
 import "fay-base" Data.Text (fromString, Text)
-import FFI (ffi, Automatic, Defined(Undefined), Nullable(Null))
+import FFI (ffi, Automatic, Defined(Defined, Undefined), Nullable(Null))
 
 import HaskellReact.Event (SyntheticMouseEvent)
 import HaskellReact
 
 data ButtonTechnicalProps = ButtonTechnicalProps {
+  disabled_ :: Defined Text ,
   type_ :: Text ,
   data_toggle :: Text ,
   aria_expanded :: Text }
@@ -21,16 +22,26 @@ data UlTechnicalProps = UlTechnicalProps {
   role :: Text }
 
 buttonDropdown :: Renderable a
-               => a -- ^ button label
+               => a
                -> [DOMElement] -- ^ list of li elements rendered in the dropdown list
                -> DOMElement
-buttonDropdown buttonLabel liElements = let
+buttonDropdown = buttonDropdown' True
+
+buttonDropdown' :: Renderable a
+                => Bool
+                -> a -- ^ button label
+                -> [DOMElement] -- ^ list of li elements rendered in the dropdown list
+                -> DOMElement
+buttonDropdown' enabled buttonLabel liElements = let
   buttonProps = ButtonTechnicalProps {
     data_toggle = "dropdown" ,
     aria_expanded = "false" ,
     type_ = "button" }
+  buttonProps' = if enabled
+    then buttonProps { disabled_ = Undefined }
+    else buttonProps { disabled_ = Defined "disabled" }
   ulProps = UlTechnicalProps {
     role = "menu" }
   in div' (class' "btn-group") [
-    constructDOMElement "button" (class'' ["btn","btn-default","dropdown-toggle"]) buttonProps buttonLabel ,
+    constructDOMElement "button" (class'' ["btn","btn-default","dropdown-toggle"]) buttonProps' buttonLabel ,
     constructDOMElement "ul" (class' "dropdown-menu") ulProps liElements ]
