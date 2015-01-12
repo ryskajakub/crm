a5b96efab04eb01aac630c90484906dc33bfde75
Jakub Ryška <ryskajakub@seznam.cz>
2015-01-12 21:30:25 +0000

    move the typeclass intstances into separate file

--- server/src/Crm/Server/Base.hs
+++ server/src/Crm/Server/Base.hs
@@ -34,10 +34,7 @@ import Control.Arrow (returnA)
 import Control.Monad (forM_, forM)
 
 import Data.Profunctor.Product (p1, p2, p3, p4, p5, p6)
-import Data.JSON.Schema.Generic (gSchema)
-import qualified Data.JSON.Schema.Types as JS (JSONSchema(schema))
-import Data.Aeson.Types (toJSON, ToJSON, FromJSON, parseJSON)
-import Data.Maybe (fromJust, maybeToList)
+import Data.Maybe (maybeToList)
 import Data.Functor.Identity (runIdentity)
 import Data.Time.Calendar (Day, addDays)
 import Data.Int (Int64)
@@ -62,12 +59,11 @@ import qualified Crm.Shared.UpkeepMachine as UM
 import qualified Crm.Shared.YearMonthDay as D
 import qualified Crm.Shared.Employee as E
 import qualified Crm.Shared.UpkeepSequence as US
-import Crm.Server.Helpers (ymdToDay, dayToYmd)
 
-import Fay.Convert (showToFay, readFromFay')
+import Crm.Server.Helpers (ymdToDay, dayToYmd)
+import Crm.Server.Boilerplate ()
 
 import Safe (readMay, minimumMay)
-import Generics.Regular
 
 import qualified Opaleye.Internal.Column as C
 import qualified Opaleye.Internal.HaskellDB.PrimQuery as HPQ
@@ -424,80 +420,6 @@ type Dependencies = (ReaderT Connection IO :: * -> *)
 type IdDependencies = (ReaderT (Connection, Either String Int) IO :: * -> *)
 type StringIdDependencies = (ReaderT (Connection, String) IO :: * -> *)
 type MachineTypeDependencies = (ReaderT (Connection, MachineTypeSid) IO :: * -> *)
-
-deriveAll ''C.Company "PFCompany"
-type instance PF C.Company = PFCompany
-
-deriveAll ''M.Machine "PFMachine"
-type instance PF M.Machine = PFMachine
-
-deriveAll ''MT.MachineType "PFMachineType"
-type instance PF MT.MachineType = PFMachineType
-
-deriveAll ''U.Upkeep "PFUpkeep"
-type instance PF U.Upkeep = PFUpkeep
-
-deriveAll ''UM.UpkeepMachine "PFUpkeepMachine"
-type instance PF UM.UpkeepMachine = PFUpkeepMachine
-
-fayInstance value = case readFromFay' value of
-  Left e -> fail e
-  Right ok -> return ok
-instance FromJSON MT.MyEither where
-  parseJSON = fayInstance
-instance FromJSON MT.MachineType where 
-  parseJSON = fayInstance
-instance FromJSON C.Company where
-  parseJSON = fayInstance
-instance FromJSON U.Upkeep where
-  parseJSON = fayInstance
-instance FromJSON UM.UpkeepMachine where
-  parseJSON = fayInstance
-instance FromJSON M.Machine where
-  parseJSON = fayInstance
-instance FromJSON US.UpkeepSequence where
-  parseJSON = fayInstance
-
--- super unsafe
-instance ToJSON D.YearMonthDay where
-  toJSON = fromJust . showToFay
-instance ToJSON C.Company where
-  toJSON = fromJust . showToFay
-instance ToJSON U.Upkeep where
-  toJSON = fromJust . showToFay
-instance ToJSON MT.MachineType where
-  toJSON = fromJust . showToFay
-instance ToJSON M.Machine where
-  toJSON = fromJust . showToFay
-instance ToJSON UM.UpkeepMachine where
-  toJSON = fromJust . showToFay
-instance ToJSON E.Employee where
-  toJSON = fromJust . showToFay
-instance ToJSON US.UpkeepSequence where
-  toJSON = fromJust . showToFay
-
-instance JS.JSONSchema E.Employee where
-  schema = gSchema
-instance JS.JSONSchema C.Company where
-  schema = gSchema
-instance JS.JSONSchema M.Machine where
-  schema = gSchema
-instance JS.JSONSchema D.YearMonthDay where
-  schema = gSchema
-instance JS.JSONSchema D.Precision where
-  schema = gSchema
-instance JS.JSONSchema MT.MachineType where
-  schema = gSchema
-instance JS.JSONSchema Char where
-  schema = gSchema
-instance JS.JSONSchema MT.MyEither where
-  schema = gSchema
-instance JS.JSONSchema U.Upkeep where
-  schema = gSchema
-instance JS.JSONSchema UM.UpkeepMachine where
-  schema = gSchema
-instance JS.JSONSchema US.UpkeepSequence where
-  schema = gSchema
 
 instance Eq D.YearMonthDay where
   D.YearMonthDay y m d _ == D.YearMonthDay y' m' d' _ = y == y' && m == m' && d == d'
---
+++ server/src/Crm/Server/Boilerplate.hs
@@ -0,0 +1,103 @@
+{-# OPTIONS -fno-warn-orphans #-}
+
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PackageImports #-}
+
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Crm.Server.Boilerplate where
+
+import Generics.Regular (deriveAll, PF)
+import Data.Aeson.Types (toJSON, ToJSON, FromJSON, parseJSON)
+import qualified Data.JSON.Schema.Types as JS (JSONSchema(schema))
+import Fay.Convert (showToFay, readFromFay')
+import Data.Maybe (fromJust)
+import Data.JSON.Schema.Generic (gSchema)
+
+import qualified Crm.Shared.Company as C
+import qualified Crm.Shared.Machine as M
+import qualified Crm.Shared.MachineType as MT
+import qualified Crm.Shared.Upkeep as U
+import qualified Crm.Shared.UpkeepMachine as UM
+import qualified Crm.Shared.YearMonthDay as D
+import qualified Crm.Shared.Employee as E
+import qualified Crm.Shared.UpkeepSequence as US
+
+deriveAll ''C.Company "PFCompany"
+type instance PF C.Company = PFCompany
+
+deriveAll ''M.Machine "PFMachine"
+type instance PF M.Machine = PFMachine
+
+deriveAll ''MT.MachineType "PFMachineType"
+type instance PF MT.MachineType = PFMachineType
+
+deriveAll ''U.Upkeep "PFUpkeep"
+type instance PF U.Upkeep = PFUpkeep
+
+deriveAll ''UM.UpkeepMachine "PFUpkeepMachine"
+type instance PF UM.UpkeepMachine = PFUpkeepMachine
+
+fayInstance value = case readFromFay' value of
+  Left e -> fail e
+  Right ok -> return ok
+instance FromJSON MT.MyEither where
+  parseJSON = fayInstance
+instance FromJSON MT.MachineType where 
+  parseJSON = fayInstance
+instance FromJSON C.Company where
+  parseJSON = fayInstance
+instance FromJSON U.Upkeep where
+  parseJSON = fayInstance
+instance FromJSON UM.UpkeepMachine where
+  parseJSON = fayInstance
+instance FromJSON M.Machine where
+  parseJSON = fayInstance
+instance FromJSON US.UpkeepSequence where
+  parseJSON = fayInstance
+
+-- super unsafe
+instance ToJSON D.YearMonthDay where
+  toJSON = fromJust . showToFay
+instance ToJSON C.Company where
+  toJSON = fromJust . showToFay
+instance ToJSON U.Upkeep where
+  toJSON = fromJust . showToFay
+instance ToJSON MT.MachineType where
+  toJSON = fromJust . showToFay
+instance ToJSON M.Machine where
+  toJSON = fromJust . showToFay
+instance ToJSON UM.UpkeepMachine where
+  toJSON = fromJust . showToFay
+instance ToJSON E.Employee where
+  toJSON = fromJust . showToFay
+instance ToJSON US.UpkeepSequence where
+  toJSON = fromJust . showToFay
+
+instance JS.JSONSchema E.Employee where
+  schema = gSchema
+instance JS.JSONSchema C.Company where
+  schema = gSchema
+instance JS.JSONSchema M.Machine where
+  schema = gSchema
+instance JS.JSONSchema D.YearMonthDay where
+  schema = gSchema
+instance JS.JSONSchema D.Precision where
+  schema = gSchema
+instance JS.JSONSchema MT.MachineType where
+  schema = gSchema
+instance JS.JSONSchema Char where
+  schema = gSchema
+instance JS.JSONSchema MT.MyEither where
+  schema = gSchema
+instance JS.JSONSchema U.Upkeep where
+  schema = gSchema
+instance JS.JSONSchema UM.UpkeepMachine where
+  schema = gSchema
+instance JS.JSONSchema US.UpkeepSequence where
+  schema = gSchema
+
