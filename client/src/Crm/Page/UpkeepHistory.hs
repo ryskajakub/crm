5e72ca416aed8e8977d46866782492714a24e4c4
Jakub Ryška <ryskajakub@seznam.cz>
2015-01-24 13:54:38 +0000

    add upkeep history display of upkeep machine data #63

--- client/src/Crm/Page/UpkeepHistory.hs
+++ client/src/Crm/Page/UpkeepHistory.hs
@@ -6,13 +6,14 @@
 module Crm.Page.UpkeepHistory (
   upkeepHistory ) where
 
-import "fay-base" Data.Text (fromString, pack)
+import "fay-base" Data.Text (fromString, pack, showInt)
 import "fay-base" Prelude hiding (div, span, id)
 
 import HaskellReact
 import qualified HaskellReact.Bootstrap as B
 
 import qualified Crm.Shared.Upkeep as U
+import qualified Crm.Shared.UpkeepMachine as UM
 import qualified Crm.Shared.Employee as E
 import Crm.Helpers (displayDate)
 import Crm.Router
@@ -21,18 +22,21 @@ upkeepHistory :: [(U.Upkeep', Maybe E.Employee')]
               -> CrmRouter
               -> DOMElement
 upkeepHistory upkeeps _ = let
-  upkeepHtml ((_, upkeep, _), maybeEmployee) = let
+  upkeepHtml ((_, upkeep, upkeepMachines), maybeEmployee) = let
     employeeText = maybe ("---") (pack . E.name . snd) maybeEmployee
     in [
       B.row $ B.col (B.mkColProps 12) (h3 $ displayDate $ U.upkeepDate upkeep) ,
       B.row $ B.col (B.mkColProps 12) [
         p [ strong "Servisman", text2DOM " " , text2DOM employeeText ] ,
-        p [ strong "Uzavřeno", text2DOM " " , 
-          text2DOM $ if U.upkeepClosed upkeep then "Ano" else "Ne" ]] , 
-      B.row $ B.col (B.mkColProps 4) $ B.panel [ h2 $ "Stroj" ,
-        dl [ 
-          dt "Poznámka" ,
-          dd "" ]]]
+        p [ strong "Uzavřeno", text2DOM " " ,
+          text2DOM $ if U.upkeepClosed upkeep then "Ano" else "Ne" ]] ,
+      B.row $ map (\(upkeepMachine,_) ->
+        B.col (B.mkColProps 4) $ B.panel [ h2 $ "Stroj" ,
+          dl [
+            dt "Poznámka" ,
+            dd $ pack $ UM.upkeepMachineNote upkeepMachine ,
+            dt "Naměřené motohodiny" ,
+            dd $ showInt $ UM.recordedMileage upkeepMachine ]]) upkeepMachines ]
   in div [
     h2 "Historie servisů" ,
     B.grid $ map upkeepHtml upkeeps ]
