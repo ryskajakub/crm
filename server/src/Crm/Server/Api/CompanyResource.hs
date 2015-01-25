1e607c0dd64342d91b17849102a9261429216023
Jakub Ryška <ryskajakub@seznam.cz>
2015-01-25 20:11:15 +0000

    make the server accept the direction of ordering #77

--- server/src/Crm/Server/Api/CompanyResource.hs
+++ server/src/Crm/Server/Api/CompanyResource.hs
@@ -10,16 +10,17 @@ import Control.Monad.Reader (ask)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad (forM)
 
-import Data.List (sortBy, find)
+import Data.List (sortBy)
 import Data.Tuple.All (sel1, sel2, sel3, uncurryN)
 
 import Rest.Resource (Resource, Void, schema, list, name, create, mkResourceReaderWith, get ,
   update )
 import qualified Rest.Schema as S
 import Rest.Dictionary.Combinators (jsonO, someO, jsonI, someI)
-import Rest.Handler (ListHandler, mkOrderedListing, mkListing, mkInputHandler, Handler, mkConstHandler)
+import Rest.Handler (ListHandler, mkOrderedListing, mkInputHandler, Handler, mkConstHandler)
 
 import qualified Crm.Shared.Company as C
+import qualified Crm.Shared.Direction as DIR
 import qualified Crm.Shared.Api as A
 import Crm.Shared.MyMaybe
 
@@ -35,8 +36,19 @@ createCompanyHandler = mkInputHandler (jsonO . jsonI . someI . someO) (\newCompa
   ask >>= \conn -> liftIO $ addCompany conn newCompany )
 
 listing :: ListHandler Dependencies
-listing = mkOrderedListing (jsonO . someO) (\(_,rawOrder,_) -> do
-  let order = (rawOrder >>= readMay)
+listing = mkOrderedListing (jsonO . someO) (\(_, rawOrder, rawDirection) -> do
+  let 
+    order = rawOrder >>= readMay
+    direction = rawDirection >>= readMay
+    -- "negate" the ordering when it is descending
+    orderingByDirection :: Ordering -> Ordering
+    orderingByDirection = case direction of
+      Nothing -> id
+      Just(DIR.Asc) -> id
+      Just(DIR.Desc) -> \ord -> case ord of
+        LT -> GT
+        GT -> LT
+        EQ -> EQ
   conn <- ask 
   rows <- liftIO $ runCompaniesQuery conn
   unsortedResult <- forM rows (\companyRow -> do
@@ -49,10 +61,11 @@ listing = mkOrderedListing (jsonO . someO) (\(_,rawOrder,_) -> do
   return $ sortBy (\r1 r2 -> case order of
     Nothing -> EQ
     Just C.CompanyName ->
-      (C.companyName $ sel2 r1) `compare` (C.companyName $ sel2 r2)
+      orderingByDirection $ (C.companyName $ sel2 r1) `compare` (C.companyName $ sel2 r2)
     Just C.NextService ->
       case (sel3 r1, sel3 r2) of
-        (MyJust (date1'), MyJust(date2')) -> date1' `compare` date2'
+        (MyJust (date1'), MyJust(date2')) -> orderingByDirection $ date1' `compare` date2'
+        -- when the date is missing, the results will always go to the bottom
         (MyNothing,MyNothing) -> EQ
         (MyNothing,_) -> GT
         _ -> LT
