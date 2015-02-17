b86cf292a2ed03376b6186c975415964ef50804f
Jakub Ryška <ryskajakub@seznam.cz>
2015-02-17 18:12:10 +0000

    add `add employee` button #89

--- client/src/Crm/Page/Employee.hs
+++ client/src/Crm/Page/Employee.hs
@@ -8,9 +8,12 @@ module Crm.Page.Employee (
 
 import "fay-base" Data.Text (fromString, pack)
 import "fay-base" Prelude hiding (div, span, id)
+import "fay-base" FFI (Defined (Defined))
 
 import HaskellReact
 import qualified HaskellReact.Bootstrap as B
+import qualified HaskellReact.Bootstrap.Button as BTN
+import qualified HaskellReact.Bootstrap.Glyphicon as G
 
 import qualified Crm.Shared.Employee as E
 
@@ -18,6 +21,12 @@ employeePage :: [(E.EmployeeId, E.Employee)]
              -> DOMElement
 employeePage employees = let 
   listEmployee employee = li $ pack $ E.name $ snd employee
+  goToAddEmployee = return ()
+  addEmployeeButton = BTN.button'
+    (BTN.buttonProps {
+      BTN.onClick = Defined $ const goToAddEmployee })
+    [G.plus, text2DOM " Přidat servismana"]
   in B.grid [
     B.row $ B.col (B.mkColProps 12) $ h2 "Servismani" ,
+    B.row $ B.col (B.mkColProps 12) $ addEmployeeButton ,
     B.row $ B.col (B.mkColProps 12) $ ul' (class' "list-unstyled") $ map listEmployee employees ]
