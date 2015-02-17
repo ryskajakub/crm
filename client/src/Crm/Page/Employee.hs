11a6f2ead2014b507d3e3d97c8ac334c65bece8b
Jakub Ryška <ryskajakub@seznam.cz>
2015-02-17 18:03:10 +0000

    list the names of employees on the Employee page #89

--- client/src/Crm/Page/Employee.hs
+++ client/src/Crm/Page/Employee.hs
@@ -6,14 +6,18 @@
 module Crm.Page.Employee (
   employeePage ) where
 
-import "fay-base" Data.Text (fromString) 
+import "fay-base" Data.Text (fromString, pack)
 import "fay-base" Prelude hiding (div, span, id)
 
 import HaskellReact
+import qualified HaskellReact.Bootstrap as B
 
 import qualified Crm.Shared.Employee as E
 
 employeePage :: [(E.EmployeeId, E.Employee)] 
              -> DOMElement
-employeePage _ =
-  div "Employees"
+employeePage employees = let 
+  listEmployee employee = li $ pack $ E.name $ snd employee
+  in B.grid [
+    B.row $ B.col (B.mkColProps 12) $ h2 "Servismani" ,
+    B.row $ B.col (B.mkColProps 12) $ ul' (class' "list-unstyled") $ map listEmployee employees ]
