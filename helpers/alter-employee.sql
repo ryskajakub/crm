cc71f240f374d0a90cc3082f6efbc5e69dada05f
Jakub Ryška <ryskajakub@seznam.cz>
2015-04-28 16:42:09 +0200

    set default for the employee added fields #117

--- helpers/alter-employee.sql
+++ helpers/alter-employee.sql
@@ -1,2 +1,2 @@
-alter table employees add contact varchar (500);
-alter table employees add capabilities varchar (500);
+alter table employees add contact varchar (500) default '';
+alter table employees add capabilities varchar (500) default '';
