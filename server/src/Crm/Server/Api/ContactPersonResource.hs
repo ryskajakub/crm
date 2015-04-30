6d5a9bedd0485ace30e94aeaa86977814c1a9de1
Jakub Ryška <ryskajakub@seznam.cz>
2015-04-30 16:20:36 +0200

    add update contact person handler #115

--- server/src/Crm/Server/Api/ContactPersonResource.hs
+++ server/src/Crm/Server/Api/ContactPersonResource.hs
@@ -1,18 +1,19 @@
 module Crm.Server.Api.ContactPersonResource (resource) where
 
-import Opaleye.RunQuery (runQuery)
+import Opaleye (runQuery, (.==), pgInt4, pgString, runUpdate)
 
 import Control.Monad.Reader (ask)
 import Control.Monad.IO.Class (liftIO)
 
-import Data.Tuple.All (sel3)
+import Data.Tuple.All (sel1, sel2, sel3)
 
-import Rest.Resource (Resource, Void, schema, list, name, mkResourceReaderWith, get)
+import Rest.Resource (Resource, Void, schema, list, name, mkResourceReaderWith, get, update)
 import qualified Rest.Schema as S
-import Rest.Dictionary.Combinators (jsonO, someO)
-import Rest.Handler (Handler, mkConstHandler)
+import Rest.Dictionary.Combinators (jsonO, someO, jsonI, someI)
+import Rest.Handler (Handler, mkConstHandler, mkInputHandler)
 
 import qualified Crm.Shared.Api as A
+import qualified Crm.Shared.ContactPerson as CP
 
 import Crm.Server.Boilerplate ()
 import Crm.Server.Types
@@ -23,6 +24,7 @@ resource :: Resource Dependencies IdDependencies UrlId Void Void
 resource = (mkResourceReaderWith prepareReaderTuple) {
   name = A.contactPersons ,
   schema = S.noListing $ S.unnamedSingle readMay' ,
+  update = Just updateHandler ,
   get = Just getHandler }
 
 getHandler :: Handler IdDependencies
@@ -32,3 +34,12 @@ getHandler = mkConstHandler (jsonO . someO) $ do
     rows <- liftIO $ runQuery connection (singleContactPersonQuery theId)
     row <- singleRowOrColumn rows
     return $ sel3 $ mapContactPerson row)
+
+updateHandler :: Handler IdDependencies
+updateHandler = mkInputHandler (jsonO . jsonI . someI . someO) (\contactPerson -> do
+  (connection, maybeInt) <- ask
+  maybeId maybeInt (\theId -> liftIO $ let
+    readToWrite row = (Nothing, sel2 row, pgString $ CP.name contactPerson,
+      pgString $ CP.phone contactPerson, pgString $ CP.position contactPerson)
+    condition row = sel1 row .== pgInt4 theId
+    in runUpdate connection contactPersonsTable readToWrite condition))
