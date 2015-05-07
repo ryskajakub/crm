863152286ef447d13cd2755a34fb0fa957b17471
Jakub Ryška <ryskajakub@seznam.cz>
2015-05-07 21:27:22 +0200

    display a may on the dashboard page #9

--- client/src/Crm/Page/Dashboard.hs
+++ client/src/Crm/Page/Dashboard.hs
@@ -8,8 +8,12 @@ module Crm.Page.Dashboard (
 
 import "fay-base" Data.Text (fromString) 
 import "fay-base" Prelude hiding (div, span, id)
+import FFI
 
 import HaskellReact
+import qualified HaskellReact.Bootstrap as B
 
 dashboard :: DOMElement
-dashboard = span "dashboard"
+dashboard = B.grid $ B.row $ B.col (B.mkColProps 12) $ 
+  div' (mkAttrs { id = Defined "half-container" }) $ 
+    div' (mkAttrs { id = Defined "dashboard-map" }) ""
