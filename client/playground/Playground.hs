4cae46ecc517f888b1f14aaa304618a56634465c
Jakub Ryška <ryskajakub@seznam.cz>
2015-05-09 08:58:56 +0200

    showcase of input with default value and value - playground

--- client/playground/Playground.hs
+++ client/playground/Playground.hs
@@ -6,23 +6,31 @@
 module Playground where
 
 import HaskellReact
-import FFI (ffi)
+import FFI
 import "fay-base" Data.Text (Text, fromString, showInt, (<>))
 import "fay-base" Prelude hiding (div)
 import "fay-base" Data.Var (Var, set, subscribeAndRead, newVar)
 
 import qualified HaskellReact.Tag.Input as I
 
+
 data State = State Int
 
-developedElement :: State -> (State -> Fay()) -> (DOMElement, Fay ())
-developedElement (State state) setState = (div' (click $ setState $ State $ state + 1) ("ahoj " <> showInt state), return ())
-  
+
+developedElement :: State -> (State -> Fay()) -> DOMElement
+developedElement (State state) setState = let
+  inputAttrs = I.mkInputAttrs {
+    I.defaultValue = Defined $ showInt (state + 10) ,
+    I.value_ = if state == 10 then Undefined else Defined $ showInt state }
+  input = I.input (click $ setState $ State $ state + 1) inputAttrs
+  in input
+
+
 renderLoop :: Fay ()
 renderLoop = do
-  appState' <- newVar $ State 0
+  appState' <- newVar $ State 10
   _ <- subscribeAndRead appState' $ \appState -> let
     mod state = set appState' state
-    (element1, callback) = developedElement appState mod
-    in simpleReactBody' element1 callback
+    element1 = developedElement appState mod
+    in simpleReactBody' element1 (return ())
   return ()
