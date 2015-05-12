13166a8672e62d8358a8c2019610c87a7ba52b87
Jakub Ryška <ryskajakub@seznam.cz>
2015-05-12 18:56:53 +0200

    add header & description to extra fields page #140

--- client/src/Crm/Page/MachineKind.hs
+++ client/src/Crm/Page/MachineKind.hs
@@ -10,16 +10,20 @@ import "fay-base" Prelude hiding (div, span, id)
 import "fay-base" Data.Var (Var, modify)
 
 import HaskellReact
+import qualified HaskellReact.Bootstrap as B
 
 import qualified Crm.Shared.MachineType as MT
 import qualified Crm.Shared.MachineKind as MK
 
 import qualified Crm.Router as R
 import qualified Crm.Data.Data as D
+import Crm.Helpers
 import Crm.Component.Form
 
 machineKindSettings :: Var D.AppState
                     -> MK.MachineKindEnum
                     -> [(MK.MachineKindEnum, [MK.MachineKindSpecific])]
                     -> DOMElement
-machineKindSettings appVar editedEnum allSettings = div ""
+machineKindSettings appVar editedEnum allSettings = let
+  header = pageInfo "Další políčka u strojů" $ Just "Tady můžeš vybrat, jaká další políčka se budou dát vyplnit u strojů. Ke každému druhu stroje můžeš přiřadit další políčka, ty se zobrazí potom na stránce stroje, kde ho vyplníš."
+  in B.grid header
