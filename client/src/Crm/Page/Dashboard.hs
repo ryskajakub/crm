658cea8c96439f7f56556fb2f91112564f30e97e
Jakub Ryška <ryskajakub@seznam.cz>
2015-05-18 09:45:49 +0200

    show the colored marker on the dashboard map #131

--- client/src/Crm/Page/Dashboard.hs
+++ client/src/Crm/Page/Dashboard.hs
@@ -6,8 +6,8 @@
 module Crm.Page.Dashboard (
   dashboard ) where
 
-import "fay-base" Data.Text (pack, (<>), Text)
-import "fay-base" Prelude hiding (div, span, id)
+import "fay-base" Data.Text (pack, (<>), Text, length)
+import "fay-base" Prelude hiding (div, span, id, length)
 import "fay-base" Data.Maybe (onJust, mapMaybe)
 import FFI
 import DOM
@@ -23,27 +23,32 @@ import qualified Crm.Shared.YearMonthDay as YMD
 import Crm.Helpers (pageInfo)
 
 
+toHexa' :: Int -> Text
+toHexa' = ffi " (%1).toString(16) "
+
+
 toHexa :: Int -> Text
-toHexa = ffi " (%1).toString(16) "
+toHexa int = let
+  hexa = toHexa' int
+  in if length hexa == 1 then pack "0" <> hexa else hexa
 
 
-computeColor :: YMD.YearMonthDay -> Fay Text
-computeColor (YMD.YearMonthDay y m d _) = do
-  let moment = M.requireMoment
-  let today = M.now moment
-  let 
-    theOtherDay = M.dayPrecision y m d moment
-    diff' = fromIntegral $ M.diff today theOtherDay M.Days
-    year = 366 :: Double
-    colorScaleSize = 256 :: Double
-    diff = if diff' > year then year else diff'
-    halfYear = year / 2 
-    unit = colorScaleSize / halfYear
-    firstPart = if diff > halfYear then halfYear else diff
-    secondPart = if diff - halfYear > 0 then diff - halfYear else 0
-    redPart = toHexa $ truncate $ colorScaleSize - secondPart * unit
-    greenPart = toHexa $ truncate $ unit * firstPart
-  return $ pack "#" <> redPart <> greenPart <> pack "00"
+computeColor :: YMD.YearMonthDay -> Text
+computeColor (YMD.YearMonthDay y m d _) = let
+  moment = M.requireMoment
+  today = M.now moment
+  theOtherDay = M.dayPrecision y m d moment
+  diff' = fromIntegral $ M.diff today theOtherDay M.Days
+  year = 366 :: Double
+  colorScaleSize = 255 :: Double
+  diff = abs $ if diff' > year then year else diff'
+  halfYear = year / 2 
+  unit = colorScaleSize / halfYear
+  firstPart = if diff > halfYear then halfYear else diff
+  secondPartDiff = if diff - halfYear > 0 then diff - halfYear else 0
+  redPart = truncate $ colorScaleSize - (secondPartDiff * unit)
+  greenPart = truncate $ unit * firstPart
+  in toHexa redPart <> toHexa greenPart <> pack "00"
   
 
 dashboard :: [(C.CompanyId, C.Company, Maybe YMD.YearMonthDay, Maybe C.Coordinates)] -> (DOMElement, Fay ())
@@ -56,7 +61,9 @@ dashboard companies = let
       companiesWithCoords = mapMaybe (\(a,b,mbYmd,coords) -> (\x -> (a,b,mbYmd,x)) `onJust` coords) companies
     mapContainer <- getElementById $ pack "dashboard-map"
     googleMap <- mkMap mapContainer mapOptions
-    mapM_ (\(_,_,_,C.Coordinates lat lng) -> addMarker lat lng googleMap) companiesWithCoords
+    forM_ companiesWithCoords $ \(_,_,date,C.Coordinates lat lng) -> let
+      color = maybe (pack "777777") computeColor date 
+      in addMarker lat lng color googleMap
     return ()
     
   info = pageInfo (pack "Nástěnka") $ Just $ pack "Mapa firem. Firma se na mapě zobrazí podle vyplněné adresy."
