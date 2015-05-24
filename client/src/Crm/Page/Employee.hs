4a85e9c1bb1e7856402719e895f21a5c63a29ca6
Jakub Ryška <ryskajakub@seznam.cz>
2015-05-24 12:09:24 +0200

    imports at employee page

--- client/src/Crm/Page/Employee.hs
+++ client/src/Crm/Page/Employee.hs
@@ -6,27 +6,28 @@ module Crm.Page.Employee (
   employeePage ,
   newEmployeeForm ) where
 
-import Data.Text (fromString, Text, length)
-import Prelude hiding (div, span, id, length)
-import FFI (Defined (Defined))
-import Data.Var (Var, modify)
+import           Data.Text                        (fromString, Text, length)
+import           Prelude                          hiding (div, span, id, length)
+import           FFI                              (Defined (Defined))
+import           Data.Var                         (Var, modify)
 
-import HaskellReact
-import qualified HaskellReact.Bootstrap as B
-import qualified HaskellReact.Bootstrap.Button as BTN
+import           HaskellReact
+import qualified HaskellReact.Bootstrap           as B
+import qualified HaskellReact.Bootstrap.Button    as BTN
 import qualified HaskellReact.Bootstrap.Glyphicon as G
 
-import Crm.Server (createEmployee, updateEmployee)
-import Crm.Component.Form
-import qualified Crm.Data.Data as D
-import qualified Crm.Data.EmployeeData as ED
-import qualified Crm.Shared.Employee as E
-import Crm.Router (CrmRouter, navigate, newEmployee)
-import qualified Crm.Router as R
-import Crm.Helpers (pageInfo, validationHtml)
+import           Crm.Server                       (createEmployee, updateEmployee)
+import           Crm.Component.Form
+import qualified Crm.Data.Data                    as D
+import qualified Crm.Data.EmployeeData            as ED
+import qualified Crm.Shared.Employee              as E
+import           Crm.Router                       (CrmRouter, navigate, newEmployee)
+import qualified Crm.Router                       as R
+import           Crm.Helpers                      (pageInfo, validationHtml)
+
 
 employeePage :: CrmRouter
-             -> [(E.EmployeeId, E.Employee)] 
+             -> [(E.EmployeeId, E.Employee)]
              -> DOMElement
 employeePage router employees = let 
   rowEmployee (employeeId, employee) = tr [ 
@@ -49,6 +50,7 @@ employeePage router employees = let
     B.row $ B.col (B.mkColProps 12) $ addEmployeeButton ,
     B.row $ B.col (B.mkColProps 12) $ B.table [ head' , body ]]
 
+
 newEmployeeForm :: CrmRouter
                 -> E.Employee
                 -> Var D.AppState
@@ -57,6 +59,7 @@ newEmployeeForm router employee = employeeForm pageInfo' (buttonLabel, buttonAct
   buttonLabel = "Přidat servismena"
   buttonAction = createEmployee employee $ navigate R.employeePage router
   pageInfo' = pageInfo "Nový servisman" $ Just "Tady můžeš přídat nového servismana, pokud najmete nového zaměstnance, nebo pokud využijete služeb někoho externího."
+
 
 employeeEdit :: E.EmployeeId
              -> CrmRouter
