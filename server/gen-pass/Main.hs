beca5005b750e2edd6734a554fb11457d1dd5e98
Jakub Ryška <ryskajakub@seznam.cz>
2015-05-26 11:14:00 +0200

    delete the old password on new password add #157

--- server/gen-pass/Main.hs
+++ server/gen-pass/Main.hs
@@ -1,16 +1,14 @@
 module Main(main) where
 
-import Opaleye (runInsert, pgStrictByteString)
+import           Data.Text                (pack)
+import           Data.Text.Encoding       (encodeUtf8)
+import           Control.Monad.IO.Class   (liftIO)
+import qualified Crypto.Scrypt            as CS
+import           Opaleye                  (runInsert, pgStrictByteString, runDelete, pgBool)
+import           System.Console.Haskeline as H
 
-import System.Console.Haskeline as H
+import           Crm.Server.DB            (passwordTable, withConnection)
 
-import Control.Monad.IO.Class (liftIO)
-import qualified Crypto.Scrypt as CS
-
-import Crm.Server.DB (passwordTable, withConnection)
-
-import Data.Text.Encoding (encodeUtf8)
-import Data.Text (pack)
 
 main :: IO ()
 main = H.runInputT H.defaultSettings $ do
@@ -20,7 +18,8 @@ main = H.runInputT H.defaultSettings $ do
       let pass = CS.Pass . encodeUtf8 . pack $ password
       H.outputStrLn "Using UTF-8 encoding"
       CS.EncryptedPass byteStringPassword <- liftIO $ CS.encryptPassIO CS.defaultParams pass
-      _ <- liftIO $ withConnection $ \connection -> 
+      _ <- liftIO $ withConnection $ \connection -> do
+        runDelete connection passwordTable (const . pgBool $ True)
         runInsert connection passwordTable $ pgStrictByteString byteStringPassword
       return "password saved into db"
     Nothing -> return "reading error"
