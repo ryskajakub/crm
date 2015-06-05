b252167ba147bef10db004d227f5f0e0f14c1260
Jakub Ryška <ryskajakub@seznam.cz>
2015-06-05 13:36:40 +0200

    compute the initial value of cache on start #168

--- server/src/Server.hs
+++ server/src/Server.hs
@@ -1,22 +1,29 @@
 module Server (
   main, api) where
 
-import Data.IORef               (newIORef)
+import           Data.IORef                 (newIORef)
+import qualified Data.Map                   as M
 
-import Control.Monad.Reader     (ReaderT, runReaderT)
+import           Control.Monad.Reader       (ReaderT, runReaderT)
+import           Control.Monad.Trans.Except (runExceptT)
 
-import Network.Wai.Handler.Warp (run)
-import Rest.Driver.Wai          (apiToApplication)
+import           Network.Wai.Handler.Warp   (run)
+import           Rest.Driver.Wai            (apiToApplication)
 
-import Crm.Server.Base
-import Crm.Server.Types
-import Crm.Server.DB
+import           Crm.Server.CachedCore      (recomputeWhole)
+import           Crm.Server.Base
+import           Crm.Server.Types
+import           Crm.Server.DB
+
 
 main :: IO ()
 main = do
   putStrLn "Starting warp server on http://localhost:8000"
-  cache <- newIORef []
-  run 8000 $ apiToApplication (runDependencies $ Cache cache) api
+  cache' <- newIORef M.empty
+  let cache = Cache cache'
+  _ <- withConnection $ \c -> runExceptT $ recomputeWhole c cache
+  run 8000 $ apiToApplication (runDependencies $ cache) api
+
 
 runDependencies :: Cache -> Dependencies a -> IO a
 runDependencies cache deps = withConnection $ \c -> runReaderT deps (cache, c)
