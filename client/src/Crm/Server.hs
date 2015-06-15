6a22e33af8d23cf365635d84fe191d2a62c15e36
Jakub Ryška <ryskajakub@seznam.cz>
2015-06-15 12:43:17 +0200

    remove the double encoding of the uri everywhere

--- client/src/Crm/Server.hs
+++ client/src/Crm/Server.hs
@@ -141,7 +141,7 @@ fetchMachineTypesManufacturer :: Text -- ^ the string user typed
                               -> Fay ()
 fetchMachineTypesManufacturer text = 
   XMT.listByAutocompleteManufacturer
-    (unpack . encodeURIComponent . encodeURIComponent $ text)
+    (unpack . encodeURIComponent $ text)
   
 fetchMachineTypesAutocomplete :: Text -- ^ the string user typed
                               -> ([Text] -> Fay ()) -- callback filled with option that the user can pick
@@ -164,7 +164,7 @@ fetchMachineType :: Text -- ^ machine type exact match
                  -> Fay ()
 fetchMachineType machineTypeName callback = 
   XMT.byByName 
-    (unpack . encodeURIComponent . encodeURIComponent $ machineTypeName)
+    (unpack . encodeURIComponent $ machineTypeName)
     (callback . toMaybe)
 
 fetchEmployees :: ([E.Employee'] -> Fay ())
