4d60747b996576e080d6854169b378dd4720b736
Jakub Ryška <ryskajakub@seznam.cz>
2015-06-21 16:36:54 +0200

    display info about machine name and contact person to print daily plan page #186

--- client/src/Crm/Page/UpkeepPrint.hs
+++ client/src/Crm/Page/UpkeepPrint.hs
@@ -4,7 +4,7 @@
 module Crm.Page.UpkeepPrint (
   upkeepPrint ) where
 
-import           Data.Text                (fromString)
+import           Data.Text                (fromString, (<>))
 import           Prelude                  hiding (div)
 
 import           HaskellReact
@@ -21,10 +21,16 @@ upkeepPrint :: [(U.Upkeep, C.Company, [E.Employee], [(M.Machine, MT.MachineType,
             -> DOMElement
 upkeepPrint data' = let
   header = h2 "Denní akce"
-  displayUpkeep (upkeep, company, employees, machinesData) = B.row $ upkeepPrintDataHeader where
+  displayUpkeep (upkeep, company, employees, machinesData) = B.row $
+    upkeepPrintDataHeader ++ 
+    (concat . rdrMachines $ machinesData) 
+    where
     upkeepPrintDataHeader = map (B.col (B.mkColProps 6)) [
       strong "Firma" , text2DOM . C.companyName $ company ,
       strong "Adresa" , text2DOM . C.companyAddress $ company ]
+    rdrMachines = map $ \(machine, machineType, contactPerson) -> map (B.col (B.mkColProps 6)) [
+      strong "Zařízení", text2DOM $ MT.machineTypeName machineType <> " " <> M.note machine <> " " <> M.serialNumber machine ,
+      strong "Kontaktní osoba", text2DOM $ CP.name contactPerson <> " " <> CP.phone contactPerson ]
   in B.grid $
     B.row $ B.col (B.mkColProps 12) header :
     map displayUpkeep data' 
