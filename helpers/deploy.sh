3db999657937380eb06d93f3c68f76dc2ce1ac1d
Jakub Ryška <ryskajakub@seznam.cz>
2015-06-26 12:49:14 +0200

    update the deploy script

--- helpers/deploy.sh
+++ helpers/deploy.sh
@@ -3,8 +3,10 @@
 ROOT_CRM='..'
 SERVER="$ROOT_CRM/server"
 CLIENT="$ROOT_CRM/client"
-tar -hcvzf crm.tgz "$SERVER/.cabal-sandbox/bin/gen-pass" "$SERVER/.cabal-sandbox/bin/run-server" "$CLIENT/build" crm
+STACK_DIR=".stack-work/install/x86_64-linux/lts-2.15/7.8.4/bin"
+INSTALL_DIR="$SERVER/$STACK_DIR"
+tar -hcvzf crm.tgz "$INSTALL_DIR/insert-pass" "$INSTALL_DIR/run-server" "$CLIENT/build" crm
 scp crm.tgz mywedos:/var/www/2e/
 ssh mywedos 'tar xvzf /var/www/2e/crm.tgz -C /var/www/2e/'
 ssh mywedos 'killall -9 run-server'
-ssh mywedos '/var/www/2e/server/.cabal-sandbox/bin/run-server'
+ssh mywedos "/var/www/2e/server/$STACK_DIR/run-server"
