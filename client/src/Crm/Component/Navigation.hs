f063cebc8dfd4e9005304ea9ab2be7c3b30cd88b
Jakub Ryška <ryskajakub@seznam.cz>
2015-07-19 16:41:28 +0200

    redirect to login after clicking on logout #199

--- client/src/Crm/Component/Navigation.hs
+++ client/src/Crm/Component/Navigation.hs
@@ -5,10 +5,12 @@ module Crm.Component.Navigation where
 
 import           Prelude                          hiding (span, div, elem)
 import           Data.Text                        (fromString)
+import           Data.LocalStorage                (removeLocalStorage)
 
 import           HaskellReact
 import           HaskellReact.Bootstrap           (navBar, nav)
 import qualified HaskellReact.Bootstrap.Glyphicon as G
+import qualified HaskellReact.Tag.Hyperlink       as A
 import qualified Moment                           as M
 
 import qualified Crm.Shared.YearMonthDay          as YMD
@@ -23,6 +25,10 @@ navigation' router (body, callbacks) = do
     moment = M.requireMoment
     (y, m, d) = M.day . M.now $ moment
     todayYMD = YMD.YearMonthDay y m d YMD.DayPrecision
+    logout = do
+      removeLocalStorage "password"
+      navigate login router
+
   simpleReactBody' ( div [
     navBar $ nav [
       li $ link [G.home, text2DOM " Seznam firem"] defaultFrontPage router ,
@@ -31,7 +37,8 @@ navigation' router (body, callbacks) = do
       li $ link [G.user, text2DOM " Servismani"] employeePage router ,
       li $ link [G.dashboard, text2DOM " Nástěnka"] dashboard router ,
       li $ link [G.asterisk, text2DOM " Speciální pole"] extraFields router ,
-      li $ link [G.print, text2DOM " Tisk denních plánů"] (dailyPlan todayYMD Nothing) router ] ,
+      li $ link [G.print, text2DOM " Tisk denních plánů"] (dailyPlan todayYMD Nothing) router ,
+      li $ A.a''' (click logout) [G.logOut, text2DOM " Odhlášení"]] ,
     div body ]) callbacks 
 
 navigation :: CrmRouter
