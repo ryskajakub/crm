2ead287849325f62afb610a1d3f4aad472d2a070
Jakub Ryška <ryskajakub@seznam.cz>
2015-11-10 13:10:50 +0100

    display table in daily plan #196

--- client/src/Crm/Page/UpkeepPrint.hs
+++ client/src/Crm/Page/UpkeepPrint.hs
@@ -4,25 +4,26 @@
 module Crm.Page.UpkeepPrint (
   upkeepPrint ) where
 
-import           Data.Text                (fromString, (<>))
-import           Prelude                  hiding (div)
+import           Data.Text                    (fromString, (<>))
+import           Prelude                      hiding (div)
 
 import           HaskellReact
-import qualified HaskellReact.Bootstrap   as B
+import qualified HaskellReact.Bootstrap       as B
+import qualified HaskellReact.Bootstrap.Table as BT
 
-import           Crm.Helpers              (displayDate, plusDays)
-import           Crm.Component.Form       (nullDropdown)
-import qualified Crm.Router                       as R
+import           Crm.Helpers                  (displayDate, plusDays)
+import           Crm.Component.Form           (nullDropdown)
+import qualified Crm.Router                   as R
 
-import qualified Crm.Shared.Company       as C
-import qualified Crm.Shared.Employee      as E
-import qualified Crm.Shared.ContactPerson as CP
-import qualified Crm.Shared.Machine       as M
-import qualified Crm.Shared.MachineType   as MT
-import qualified Crm.Shared.Upkeep        as U
-import qualified Crm.Shared.UpkeepMachine as UM
-import qualified Crm.Shared.ServerRender  as SR
-import qualified Crm.Shared.YearMonthDay  as YMD
+import qualified Crm.Shared.Company           as C
+import qualified Crm.Shared.Employee          as E
+import qualified Crm.Shared.ContactPerson     as CP
+import qualified Crm.Shared.Machine           as M
+import qualified Crm.Shared.MachineType       as MT
+import qualified Crm.Shared.Upkeep            as U
+import qualified Crm.Shared.UpkeepMachine     as UM
+import qualified Crm.Shared.ServerRender      as SR
+import qualified Crm.Shared.YearMonthDay      as YMD
 
 upkeepPrint :: R.CrmRouter
             -> YMD.YearMonthDay
@@ -40,12 +41,12 @@ upkeepPrint router day employeeId data' employees = let
     \eId -> R.navigate (R.dailyPlan day eId) router
   header = h2 $ "Denní akce - " <> displayDate day
   displayUpkeep (_, company, _, machinesData) = div' (class'' ["row", "print-company"]) $
-    upkeepPrintDataHeader ++ 
+    (B.col (B.mkColProps 12) upkeepPrintDataHeader) :
     (concat . rdrMachines $ machinesData) 
     where
-    upkeepPrintDataHeader = map (B.col (B.mkColProps 6)) [
-      strong "Firma" , text2DOM . C.companyName $ company ,
-      strong "Adresa" , text2DOM . C.companyAddress $ company ]
+    upkeepPrintDataHeader = BT.table (Just BT.Bordered) [
+      tr [th "Firma", td (text2DOM . C.companyName $ company)] ,
+      tr [th "Adresa", td (text2DOM . C.companyAddress $ company)]]
     rdrMachines = map $ \(machine, machineType, contactPerson, (upkeepMachine, markup')) -> let
       upkeepMachineText = case markup' of
         Just (markup) -> div . render $ markup
