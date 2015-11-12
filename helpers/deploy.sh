66e461aca10ee80da9c9d92a2ec3c7d8f63c48dc
Jakub Ryška <ryskajakub@seznam.cz>
2015-11-12 08:12:50 +0100

    update snapshot definition in deploy #205

--- helpers/deploy.sh
+++ helpers/deploy.sh
@@ -3,7 +3,7 @@
 ROOT_CRM='..'
 SERVER="$ROOT_CRM/server"
 CLIENT="$ROOT_CRM/client"
-STACK_DIR=".stack-work/install/x86_64-linux/lts-2.15/7.8.4/bin"
+STACK_DIR=".stack-work/install/x86_64-linux/lts-3.13/7.10.2/bin"
 INSTALL_DIR="$SERVER/$STACK_DIR"
 tar -hcvzf crm.tgz "$INSTALL_DIR/insert-pass" "$INSTALL_DIR/run-server" "$CLIENT/build" crm
 scp crm.tgz mywedos:/var/www/2e/
