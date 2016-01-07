36cebb9abdd82fe3e5a0e0ac76c0c3d2c822be57
Jakub Ryška <ryskajakub@seznam.cz>
2016-01-07 15:07:04 +0100

    add full-option bootstrap button variant

--- wrappers/src/HaskellReact/Bootstrap/Button.hs
+++ wrappers/src/HaskellReact/Bootstrap/Button.hs
@@ -6,10 +6,11 @@
 module HaskellReact.Bootstrap.Button where
 
 import "fay-base" Prelude
-import "fay-base" Data.Text (fromString, Text)
-import "fay-base" FFI (Defined(Undefined))
+import "fay-base" Data.Text (fromString, Text, (<>))
+import "fay-base" FFI (Defined(Undefined, Defined))
 
-import HaskellReact (Renderable, DOMElement)
+import qualified HaskellReact as HR
+import qualified HaskellReact.Tag.Controls as HC
 import HaskellReact.Event (SyntheticMouseEvent)
 import HaskellReact.Bootstrap (reactBootstrap)
 
@@ -26,13 +27,44 @@ buttonProps = ButtonProps {
   disabled = Undefined ,
   onClick = Undefined }
 
-button' :: Renderable a
-        => ButtonProps
-        -> a
-        -> DOMElement
+button' :: 
+  HR.Renderable a => 
+  ButtonProps -> 
+  a -> 
+  HR.DOMElement
 button' props children = reactBootstrap "Button" props children
 
-button :: Renderable a
-       => a
-       -> DOMElement
+button :: 
+  HR.Renderable a => 
+  a -> 
+  HR.DOMElement
 button = button' buttonProps
+
+data ButtonStyle = PrimaryButton | DefaultButton
+
+data ButtonSize = LargeButton | NormalButton | SmallButton | ExtraSmallButton
+
+expressStyle :: ButtonStyle -> Text
+expressStyle PrimaryButton = "primary"
+expressStyle DefaultButton = "default"
+
+expressSize :: ButtonSize -> Text
+expressSize LargeButton = "lg"
+expressSize NormalButton = ""
+expressSize SmallButton = "sm"
+expressSize ExtraSmallButton = "xs"
+
+buttonP :: 
+  HR.Renderable a =>
+  ButtonSize ->
+  ButtonStyle ->
+  (SyntheticMouseEvent -> Fay ()) ->
+  a ->
+  HR.DOMElement
+buttonP buttonSize buttonStyle clickHandler title =
+  HC.button' buttonAttributes title 
+  where
+  buttonAttributes = (HR.class'' ["btn", "btn-" <> expressSize buttonSize, "btn-" <> expressStyle buttonStyle]) {
+    HR.onClick = Defined clickHandler }
+
+
