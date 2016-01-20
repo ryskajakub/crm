8f7b8b4c316997f4e97ff49ac6e40be0c9df6918
Jakub Ryška <ryskajakub@seznam.cz>
2016-01-20 13:13:42 +0100

    display triplets of dates on the upkeep history page #125

--- client/src/Crm/Page/UpkeepHistory.hs
+++ client/src/Crm/Page/UpkeepHistory.hs
@@ -36,6 +36,14 @@ import           Crm.Server                    (deleteUpkeep, reopenUpkeep, dele
 import qualified Crm.Runtime                   as Runtime
 
 
+byThrees :: [a] -> [[a]]
+byThrees list = let
+  go :: [a] -> [[a]] -> [[a]]
+  go inputList acc = if not . null $ inputList
+    then go (drop 3 inputList) (acc ++ [take 3 inputList])
+    else acc
+  in go list []
+
 upkeepHistory :: 
   [(U.UpkeepId, U.Upkeep2Markup, [(UM.UpkeepMachineMarkup, M.Machine, MT.MachineType, M.MachineId)], [E.Employee'], [P.PhotoId])] -> 
   [(M.MachineId, M.Machine, MT.MachineTypeId, MT.MachineType)] ->
@@ -49,100 +57,15 @@ upkeepHistory upkeepsInfo machinesInCompany companyId deletable var router = let
   basicDeleteButtonProps = BTN.buttonProps {
     BTN.disabled = Defined . not $ deletable }
 
-  upkeepRenderHtml (upkeepId, upkeep, upkeepMachines, employees, photos) = 
-    ([generalUpkeepInfo, notes, upkeepMachinesInfo] ++ photoHtml, fetchPhotos) where
+  upkeepRenderHtml3 ((upkeep1:restUpkeeps)) = 
+    ([header]) where
 
-    generalUpkeepInfo = B.row' marginTop [
-      B.col (B.mkColProps 4) [
-        text2DOM $ (<> " ") $ displayDate $ U.upkeepDate upkeep ,
-        span' (class'' ["label", "upkeep-state" , labelClass]) labelText ] ,
-      let 
-        i = if length employees > 1 then "i" else ""
-        in B.col (B.mkColProps 4) [ strong $ "Servisman" <> i <> ": ", text2DOM employeeText ] ,
-      B.col (B.mkColProps 3) $ formLink , 
-      B.col (B.mkColProps 1) $ deleteButton ] where
-        employeeText = intercalate " " . map (\(_, employee) -> E.name employee) $ employees
-        (labelClass, labelText, formLink) = if U.upkeepClosed upkeep
-          then ("label-success", "Uzavřený", let
-            buttonProps = (BTN.buttonProps {
-              BTN.bsStyle = Defined "warning" ,
-              BTN.onClick = Defined $ const clickHandler })
-            clickHandler = reopenUpkeep upkeepId goEditUpkeep router
-            goEditUpkeep = navigate (upkeepDetail upkeepId) router
-            in BTN.button' buttonProps "Otevřít")
-          else ("label-warning", "Naplánovaný", link "Uzavřít" (upkeepDetail upkeepId) router) 
-        marginTop attributes = let
-          previousClassname = className attributes
-          newClassname = case previousClassname of
-            Defined text -> Defined $ text <> " upkeep-row"
-            _ -> Defined "upkeep-row"
-          in attributes { className = newClassname }
-        deleteButton = let
-          clickHandler = deleteUpkeep upkeepId BR.refresh router
-          buttonProps = basicDeleteButtonProps {
-            BTN.bsStyle = Defined "danger" ,
-            BTN.onClick = Defined $ const clickHandler }
-          in span' (class' "delete") $ BTN.button' buttonProps "Smazat"
+    header = B.row ([
+      B.col ((B.mkColProps 3) { B.mdOffset = Defined 3 }) . displayDate . U.upkeepDate $ upkeep1 ] ++ 
+      map (B.colSize 3 . displayDate . U.upkeepDate) restUpkeeps)
 
-    notes = B.row [
-      mkCol note , 
-      mkCol recommendation ] where
-        mkCol = B.col (B.mkColProps 6)
-        note = [
-          h3 "Popis servisu" ,
-          noteContent ]
-        recommendation = [
-          h3 "Doporučení" ,
-          recommendationContent ]
-        noteContent = div . renderMarkup . U.workDescription $ upkeep
-        recommendationContent = div . renderMarkup . U.recommendation $ upkeep
-
-    mkLineUpkeepMachineInfo (upkeepMachine, machine, machineType, machineId) =
-      B.col (B.mkColProps 6) $ B.panel [ h3 $ link 
-        (displayMachine machine machineType)
-        (machineDetail machineId)
-        router ,
-        dl [[
-          dt "Plánované úkony" ,
-          dd . renderMarkup . UM.upkeepMachineNote $ upkeepMachine ,
-          dt "Závěry po servisu" ,
-          dd . renderMarkup . UM.endNote $ upkeepMachine ] ++ 
-          (if U.upkeepClosed upkeep then (if MT.kind machineType == MK.RotaryScrewCompressor then [
-          dt "Naměřené motohodiny" ,
-          dd $ showInt $ UM.recordedMileage upkeepMachine] else []) ++ [
-          dt "Záruka" ,
-          dd $ (if UM.warrantyUpkeep upkeepMachine then "Ano" else "Ne") ] else []) ]]
-    upkeepMachinesInfo = B.row $ map mkLineUpkeepMachineInfo upkeepMachines
-    photoHtml = map mkPhotoRow photos
-
-    mkPhotoRow photoId = let
-      deleteButton = let
-        buttonProps = basicDeleteButtonProps {
-          BTN.bsStyle = Defined "danger" ,
-          BTN.onClick = Defined $ const clickHandler }
-        clickHandler = deletePhoto photoId reload router
-        in BTN.button' buttonProps [G.arrowDown, text2DOM " Smazat fotku"]
-      in B.row [ 
-      B.col ((B.mkColProps 2) { B.mdOffset = Defined 10 }) deleteButton ,
-      B.fullCol $ IMG.image' 
-        (mkAttrs { id = Defined . (<>) "photo-" . showInt . P.getPhotoId $ photoId})
-        ((IMG.mkImageAttrs "") { IMG.width = Defined 1140 }) ]
-
-    fetchPhotos = forM_ photos $ \photoId -> Runtime.passwordAjax
-      (pack A.photos <> "/" <> (showInt . P.getPhotoId $ photoId))
-      (\imageData -> do
-        let photoHTMLId = ((<>) "#photo-" . showInt . P.getPhotoId $ photoId)
-        photoHtmlElement <- JQ.select photoHTMLId
-        _ <- JQ.setAttr "src" ("data:image/jpeg;base64," <> imageData) photoHtmlElement 
-        return ())
-      Nothing
-      Runtime.get
-      Nothing
-      Nothing
-      router
-
-  upkeepsHtml = map upkeepRenderHtml upkeepsInfo
-  flattenedUpkeepsHtml = foldl (++) [] . map fst $ upkeepsHtml
+  upkeepsHtml = map upkeepRenderHtml3 . byThrees . map (\(_,u2,_,_,_) -> u2) $ upkeepsInfo
+  flattenedUpkeepsHtml = foldl (++) [] upkeepsHtml
   
   header = B.row $ B.col (B.mkColProps 12) (h2 "Historie servisů")
   linkToCompany = B.row $ B.col (B.mkColProps 12) $
