2242a5b1af0bf69b17ede980a734b7f7071056d7
Jakub Ryška <ryskajakub@seznam.cz>
2016-10-25 12:21:49 +0200

    change lts

--- helpers/deploy.sh
+++ helpers/deploy.sh
@@ -6,7 +6,7 @@ ROOT_CRM='..'
 SERVER="$ROOT_CRM/server"
 CLIENT="$ROOT_CRM/client"
 DESTINATION=vpsfree.2e.cz
-STACK_DIR=".stack-work/install/x86_64-linux/lts-3.13/7.10.2/bin"
+STACK_DIR=".stack-work/install/x86_64-linux/lts-7.3/8.0.1/bin"
 INSTALL_DIR="$SERVER/$STACK_DIR"
 tar -hcvzf crm.tgz "$INSTALL_DIR/insert-pass" "$INSTALL_DIR/run-server" "$CLIENT/build"
 echo $PASS2E | ssh -t $DESTINATION 'sudo -S mkdir -p /var/www/2e/'
